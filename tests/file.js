f86e7b5491ccdb82256b703b6612f1fc936de7bf
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-08-27 16:55:12 +0000

    - test for the File class

---
+++ tests/file.js
@@ -0,0 +1,20 @@
+exec('deflib.js');
+LoadModule('jsnspr');
+
+var file = new File('file_test.txt');
+
+if ( file.exist ) {
+	file.Open( File.RDONLY );
+	print( 'file content:\n'+file.Read() );
+	file.Close();
+}
+
+try {
+
+file.Open( File.CREATE_FILE + File.RDWR + File.APPEND );
+file.Write('this is a test\n');
+print( 'current file pos: '+file.Seek() );
+//file.Write('x');
+file.Close();
+
+} catch ( ex if ex instanceof NSPRError ) { print( ex.text ) }
