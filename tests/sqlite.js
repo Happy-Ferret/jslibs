1ab6b8d865d90c539e3ead5a0badf272a24ccac9
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-08-28 21:33:30 +0000

    - enhance sqlite test

--- tests/sqlite.js
+++ tests/sqlite.js
@@ -1,15 +1,24 @@
 exec('deflib.js');
 LoadModule('jssqlite');
 
+try {
 
-print('database version: '+Database.version,'\n' );
+	print('database version: ' + Database.version ,'\n' );
 
-var db = new Database('test_database');
-var result = db.Query(arguments[1]);
+	var db = new Database('test_database');
+	var result = db.Query(arguments[1]);
 
-print( 'last insert row ID :'+db.lastInsertRowid,'\n' );
-print( 'col names :'+result.columnNames.toSource(),'\n' );
+	print( 'last insert row ID :'+db.lastInsertRowid ,'\n' );
+	print( 'col names          :'+result.columnNames.toSource() ,'\n' );
+	print( 'col indexes        :'+result.columnIndexes.toSource() ,'\n' );
+	print( 'first row          :'+result.Row().toSource() ,'\n' );
+	print( 'first col only     :'+result.Col(0).toSource() ,'\n' );
 
-print( result.Row().toSource() );
-result.Close();
-db.Close();
+	result.Close();
+	db.Close();
+
+} catch ( ex if ex instanceof SqliteError ) {
+	print( 'SqliteError: ' + ex.text + '('+ex.code+')' );
+
+
+}
