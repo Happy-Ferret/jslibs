31228cc2efbf6beda0b8380e51c89c2449c11838
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-08-29 12:08:50 +0000

    - modification of the sqlite test

--- tests/sqlite.js
+++ tests/sqlite.js
@@ -1,25 +1,34 @@
 exec('deflib.js');
 LoadModule('jssqlite');
 
-try {
+//try {
 
 	print('database version: ' + Database.version ,'\n' );
 
 	var db = new Database('test_database');
 	var result = db.Query(arguments[1]);
+	print( 'column count       :'+result.columnCount ,'\n' );
+	
 
+//	print( 'step result        :'+result.Step() ,'\n' );
 	print( 'last insert row ID :'+db.lastInsertRowid ,'\n' );
 	print( 'changes            :'+db.changes ,'\n' );
 	print( 'col names          :'+result.columnNames.toSource() ,'\n' );
 	print( 'col indexes        :'+result.columnIndexes.toSource() ,'\n' );
-	print( 'first row          :'+result.Row().toSource() ,'\n' );
-	print( 'first col only     :'+result.Col(0).toSource() ,'\n' );
+	
+
+	var r;
+//	while( r = result.Row() )
+//		print( 'row          :'+r.toSource() ,'\n' );
+	
+//	print( 'first col only     :'+result.Col(0).toSource() ,'\n' );
+
+
 
 	result.Close();
 	db.Close();
 
-} catch ( ex if ex instanceof SqliteError ) {
-	print( 'SqliteError: ' + ex.text + '('+ex.code+')' );
 
-
-}
+//} catch ( ex if ex instanceof SqliteError ) {
+//	print( 'SqliteError: ' + ex.text + '('+ex.code+')' );
+//}
