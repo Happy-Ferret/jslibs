223b7599b8561b7ee087508ef4336af120a60b0f
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-09-01 16:07:20 +0000

    - enhance

--- tests/objex.js
+++ tests/objex.js
@@ -1,80 +1,75 @@
 exec('deflib.js');
 LoadModule('jsobjex');
 
+function newNode(parent) {
 
-/*
-function add( what, obj, id, val, aux ) {
-
-	id != 'add' && id != 'del' && id != 'set' && 'add' in obj && obj.add(id);
-	return val;
-}
-
-function set( what, obj, id, val, aux ) {
-
-	id != 'add' && id != 'del' && id != 'set' && 'set' in obj && obj.set(id);
-	return val;
-}
-
-function del( what, obj, id, val, aux ) {
-
-	id != 'add' && id != 'del' && id != 'set' && 'del' in obj && obj.del(id);
-	return val;
+	return new objex(undefined,undefined,get,undefined,{parent:parent});
 }
 
 function get( what, obj, id, val, aux ) {
-
-	return id in obj ? val : obj[id] = new objex( add,del,get,set );
-}
-
-var o = new objex( add,del,get,set,{} );
-*/
-
-
-var handlerName = '__handler';
-
-function handler( what, obj, id, val, aux ) {
-//print('('+id+' '+val+')');
-
-	if ( id != handlerName && handlerName in obj && !(obj instanceof objex)  )
-		for ( var [i,v] in obj[handlerName] )
-			v.apply( this, arguments)
-	return val;
-}
-
-function get( what, obj, id, val, aux ) {
-
-	return id in obj ? val : obj[id] = new objex( handler,handler,get,handler );
-}
-
-function listen( obj, callback ) {
 	
-	handlerName in obj || ( obj[handlerName] = []);
-	obj[handlerName].push( callback );
-	hideProperties( obj, handlerName );
+	return id in obj ? val : obj[id] = newNode(obj);
+}
+
+function addListener( path, listener ) {
+
+	objex.Aux( path ).listener = listener;
+}
+
+function setData( path, data ) {
+	
+	var aux = objex.Aux( path );
+	aux.listener && aux.listener('set', data);
+	aux.data = data;
+	
+	while ( path = (aux=objex.Aux(path)).parent || undefined )
+		aux.listener && aux.listener('set (children)');
+}
+
+function getData( path, data ) {
+
+	return objex.Aux( path ).data || undefined;
+}
+
+function moveData( path, newPath ) {
+	
+	setData( newPath, getData(path));
+	delete objex.Aux(path).data;
+	for ( i in path )
+		moveData( path[i], newPath[i] );		
+}
+
+function delData( path ) { // delete datas but not listeners
+
+	var aux = objex.Aux(path);
+	aux.listener && aux.listener('del');
+	delete aux.data;
+	for ( var [k,v] in path )
+		delData( v );
 }
 
 
-var root = new objex( handler,handler,get,handler );
-
-listen( root.server, function( what, obj, id, val, aux ) { print('event '+val+'!\n'); } );
-
-root.server.port = 654;
-
-print(root.server.ip);
-root.server.ip = 6667;
-
-/*
-o.a.b.c.d.e.add = function(v) { print('add'+v+'\n') }
-o.a.b.c.d.e.del = function(v) { print('del'+v+'\n') }
-o.a.b.c.d.e.set = function(v) { print('set'+v+'\n') }
-
-hideProperties( o.a.b.c.d.e, 'add', 'del', 'set' );
-
-o.a.b.c.d.e.toto = 123;
-o.a.b.c.d.e.tata = 555;
-o.a.b.c.d.e.tata = 555;
-delete o.a.b.c.d.e.toto
+///////////////////////
 
 
-for ( var [k,v] in o.a.b.c.d.e ) print( v );
-*/
+var root = new objex(undefined,undefined,get,undefined,{});
+
+addListener( root.server.port.x.y, function(info) { print(info+'\n') } );
+
+setData( root.server.port, 8080 );
+//setData( root.server.ip, '127.0.0.1' );
+
+delData( root.server.port );
+print( getData( root.server.port ), '\n' );
+
+setData( root.server.port, 8081 );
+print( getData( root.server.port ), '\n' );
+
+
+moveData( root.server.port, root.server2.port );
+
+print( getData( root.server2.port ), '\n' );
+
+
+setData( root.server.port.x.y.z, 'serverNode' );
+
