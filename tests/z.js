05fb2983b9f871e1503d5cdf5823fbdd2f809db7
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-09-05 22:23:52 +0000

    - misc. enhancements

--- tests/z.js
+++ tests/z.js
@@ -1,19 +1,24 @@
 exec('deflib.js');
 LoadModule('jsz');
 
+
+//for (var i=0; i<10000; i++);
+
 try {
 
 var compressor = new Z(Z.DEFLATE);
-var res = compressor.Transform( '111111111111111111111111111111111111111111111111111111111111111' );
+var res = compressor.Transform( 'hello ' );
+res += compressor.Transform( 'world.' );
 res += compressor.Transform();
 
-//var compressor2 = new Z(Z.INFLATE);
-//var res2 = compressor2.Transform( res );
-//res2 += compressor2.Transform();
-//compressor2.Finalize();
 
-print( res.length );
+var compressor2 = new Z(Z.INFLATE);
+var res2 = compressor2.Transform( res );
+res2 += compressor2.Transform();
 
-} catch (ex) {
+
+print( '['+res2+']' );
+
+} catch (ex if ex instanceof ZError) {
 	print( ex.const );
 }
