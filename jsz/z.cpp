da81a932d498d1ae06025f1e0afe405996a160b8
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-09-07 21:33:58 +0000

    - enhancements

--- jsz/z.cpp
+++ jsz/z.cpp
@@ -94,47 +94,41 @@ JSBool z_transform(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval 
 	int method;
 	JS_GetReservedSlot( cx, obj, 0, &jsvalMethod );
 	method = JSVAL_TO_INT(jsvalMethod);
+
 // prepare input data
-	int inputLength;
-	int flushType;
-	if ( argc != 0 ) {
+	char *inputData = NULL;
+	size_t inputLength = 0;
+	if ( argc >= 1 ) {
 
 		JSString *jssData = JS_ValueToString( cx, argv[0] );
 		inputLength = JS_GetStringLength( jssData );
-		stream->next_in = (Bytef *)JS_GetStringBytes( jssData ); // no copy is done, we read directly in the string hold by SM
-		flushType = Z_SYNC_FLUSH; // Z_SYNC_FLUSH ensure that input datas will be entierly consumed
-	} else {
-		
-		stream->next_in = NULL;
-		inputLength = 0;
-		flushType = Z_FINISH;
+		inputData = JS_GetStringBytes( jssData ); // no copy is done, we read directly in the string hold by SM
 	}
-
 	stream->avail_in = inputLength;
-	Buffer buffer( method == DEFLATE ? 12 + inputLength + inputLength / 1000 : inputLength + inputLength / 2 ); // if DEFLATE, dest. buffer must be at least 0.1% larger than sourceLen plus 12 bytes
-// main loop
+	stream->next_in = (Bytef*)inputData;
+	
+	int flushType = inputLength == 0 ? Z_FINISH : Z_SYNC_FLUSH;
+	Buffer buffer( method == DEFLATE ? 12 + 1.001f * stream->avail_in : 1.25f * stream->avail_in ); // if DEFLATE, dest. buffer must be at least 0.1% larger than sourceLen plus 12 bytes
+// deflate / inflate loop
 	int status;
 	do {
-
-//		ASSERT( buffer.avail > 0 ); // Before the call of inflate()/deflate(), the application should ensure that at least one of the actions is possible, by providing more input and/or consuming more output,
 		BufferChunk *chunk = buffer.Next(buffer.SmartLength());
 		stream->avail_out = chunk->avail;
-		stream->next_out = (Bytef*)chunk->data;
-		status = method == DEFLATE ? deflate( stream, flushType ) : inflate( stream, flushType ); // compress or uncompress
+		stream->next_out = chunk->data;
+		status = method == DEFLATE ? deflate( stream, flushType ) : inflate( stream, flushType ); // Before the call of inflate()/deflate(), the application should ensure that at least one of the actions is possible, by providing more input and/or consuming more output, ...
 		chunk->avail = stream->avail_out;
-		chunk->data = (char*)stream->next_out;
-		//	printf("DE=%d, i=%d, lost= %d / %d, in=%d, avail_in=%d, avail_out=%d, totalIn=%d, totalOut=%d, ratio=%.2f \n", method == DEFLATE, queueEndIndex, stream->avail_out, chunkSize, inputLength, stream->avail_in, stream->avail_out, stream->total_in, stream->total_out, (float)stream->total_in / (float)stream->total_out );
+		chunk->data = stream->next_out;
+//		printf("D%d, ca%d ai%d ao%d ti%d to%d", method == DEFLATE, chunk->avail, stream->avail_in, stream->avail_out, stream->total_in, stream->total_out );
+//		printf("..ai%d ca%d ao%d ti%d to%d\n", 			method == DEFLATE, 			chunk->avail,			stream->avail_in, 			stream->avail_out, 			stream->total_in, 			stream->total_out		);
 		if ( status < 0 )
 			return ThrowZError( cx, status, stream->msg );
-
-	} while ( ( flushType != Z_FINISH && stream->avail_in > 0 )  ||  ( flushType == Z_FINISH && status != Z_STREAM_END ) ); // while the input data are not exhausted or the last datas are not read
-
+	} while ( ( flushType != Z_FINISH && stream->avail_in > 0 ) || ( flushType == Z_FINISH && status != Z_STREAM_END ) ); // while the input data are not exhausted or the last datas are not read
 
 // assamble chunks
 	size_t resultLength = buffer.MergeLength();
-	char *stringData = (char*)JS_malloc( cx, resultLength );
+	unsigned char *stringData = (unsigned char*)JS_malloc( cx, resultLength );
 	buffer.Merge(stringData);
-	*rval = STRING_TO_JSVAL(JS_NewString( cx, stringData, resultLength ));
+	*rval = STRING_TO_JSVAL(JS_NewString( cx, (char*)stringData, resultLength ));
 
 // close the stream and free resources
 	if ( flushType == Z_FINISH ) {
