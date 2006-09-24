d7eae2b66ae64d00707ed8b580530b83b66054dc
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-09-24 22:22:58 +0000

    - this class is used for encrypt/decrypt operations. It define CTR & CFB modes

--- jscrypt/cipher.cpp
+++ jscrypt/cipher.cpp
@@ -3,12 +3,21 @@
 #define XP_WIN
 #include <jsapi.h>
 
+#include "cryptError.h"
+
 #include "cipher.h"
 
 #include <tomcrypt.h>
 
+#include "../common/jshelper.h"
+
+#define SLOT_CIPHERNAME 0
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void cipher_Finalize(JSContext *cx, JSObject *obj) {
+
+	//free( pskey )
+	//done()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,104 +25,102 @@ JSBool cipher_call(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval 
 
 	JSObject *thisObj = JSVAL_TO_OBJECT(argv[-2]); // get 'this' object of the current object ... [TBD]: check JS_InstanceOf( cx, thisObj, &NativeProc, NULL )
 
-
-
-
-/*
-
-unsigned char key[8];
-symmetric_key skey;
-int err;
-// you must register a cipher before you use it 
-if (register_cipher(&blowfish_desc)) == -1) {
-printf("Unable to register Blowfish cipher.");
-return -1;
-}
-// generic call to function (assuming the key in key[] was already setup) 
-if ((err = cipher_descriptor[find_cipher("blowfish")].setup(key, 8, 0, &skey)) !=
-CRYPT_OK) {
-printf("Error setting up Blowfish: %s\n", error_to_string(err));
-return -1;
-}
-
-*/
-
 	return JS_TRUE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSClass cipher_class = { "cipher", JSCLASS_HAS_PRIVATE,
-  JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
-  JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, cipher_Finalize,
+JSClass cipher_class = { "Cipher", JSCLASS_HAS_PRIVATE | JSCLASS_HAS_RESERVED_SLOTS(1),
+	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
+	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, cipher_Finalize,
    0,0, cipher_call
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSBool cipher_construct(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
 
-	if ( !JS_IsConstructing(cx) ) {
-
-		JS_ReportError( cx, "construction is needed" );
-		return JS_FALSE;
-	}
-
-	if ( argc < 2 ) {
-
-		JS_ReportError( cx, "argument is missing" );
-		return JS_FALSE;
-	}
-
-
-	char *cipherName = JS_GetStringBytes( JSVAL_TO_STRING(argv[0]) );
-	if ( cipherName == NULL ) {
-
-		JS_ReportError( cx, "unable to create cipher string" );
-		return JS_FALSE;
-	}
-
-	int cipherIdx = find_cipher(cipherName);
-	if ( cipherIdx < 0 ) {
-
-		JS_ReportError( cx, "cipher %s is not registred", cipherName );
-		return JS_FALSE;
-	}
-
-	jsval jsvalKey = JSVAL_TO_STRING(argv[1]);
-	char *key = JS_GetStringBytes( jsvalKey );
-	if ( key == NULL ) {
-
-		JS_ReportError( cx, "unable to create key string" );
-		return JS_FALSE;
-	}
-
-	int keyLength = jsvalKey
-
-unsigned char key[8];
-symmetric_key skey;
-int err;
-
-
-	int err;
-	err = cipher_descriptor[cipherIdx].setup(key, 8, 0, &skey);
-
-	if ( err != CRYPT_OK)
-		return ThrowCryptError(err);
+	RT_ASSERT( JS_IsConstructing(cx), RT_ERROR_NEED_CONSTRUCTION );
+	RT_ASSERT_ARGC( 2 );
+	RT_ASSERT_CLASS( obj, &cipher_class );
 	
+	char *cipherName;
+	RT_JSVAL_TO_STRING( argv[0], cipherName );
+
+	JS_SetReservedSlot( cx, obj, SLOT_CIPHERNAME, argv[0] );
 
 
+	int cipherIdx = find_hash(cipherName);
+	RT_ASSERT_1( cipherIdx != -1, "cipher %s is not registred", cipherName )
+
+	char *key;
+	int keyLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[1], key, keyLength );
+
+	symmetric_key *pskey = (symmetric_key*)malloc(sizeof(symmetric_key));
+//	symmetric_key skey;
+
+	JS_SetPrivate( cx, obj, pskey );
+
+	int setupError = cipher_descriptor[cipherIdx].setup((const unsigned char *)key, keyLength, 0, pskey);
+	if ( setupError != CRYPT_OK)
+		return ThrowCryptError(cx, setupError);
 
 	return JS_TRUE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSBool cipher_myFunction(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+JSBool cipher_encrypt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+	
+
+	RT_ASSERT_ARGC( 1 );
+
+	jsval jsvalCipherName;
+	JS_GetReservedSlot( cx, obj, SLOT_CIPHERNAME, &jsvalCipherName );
+
+	char *cipherName;
+	RT_JSVAL_TO_STRING( jsvalCipherName, cipherName );
+
+	int cipherIdx = find_cipher(cipherName);
+	RT_ASSERT_1( cipherIdx != -1, "cipher %s is not registred", cipherName );
+
+	ltc_cipher_descriptor cipher = cipher_descriptor[cipherIdx];
+
+	symmetric_key *pskey = (symmetric_key*)JS_GetPrivate(cx,obj); // malloc(sizeof(symmetric_key));
+	RT_ASSERT( pskey != NULL, "scheduled key not initialized" );
+
+	char *pt;
+	int ptLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[0], pt, ptLength );
+
+//	int ptBlocks = 1 + ptLength / cipher.block_length;
+
+	int ctLength = (ptLength/8)*8;
+
+	char *ct = (char*)JS_malloc( cx, ctLength +1 ); // +1 is useless [TBD]
+	RT_ASSERT( ct != NULL, RT_ERROR_OUT_OF_MEMORY );
+
+	int setupError = cipher.accel_ecb_encrypt((const unsigned char *)pt, (unsigned char *)ct, ctLength, pskey);
+	if ( setupError != CRYPT_OK )
+		return ThrowCryptError(cx, setupError);
+
+	ct[ctLength] = 0; // useless because ct will contain 0x00 in its data [TBD]
+
+	JSString *jssCt = JS_NewString( cx, ct, ctLength );
+
+	*rval = STRING_TO_JSVAL(jssCt);
+
+	return JS_TRUE;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+JSBool cipher_decrypt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
 
 	return JS_TRUE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSFunctionSpec cipher_FunctionSpec[] = { // *name, call, nargs, flags, extra
- { "myFunction"          , cipher_myFunction          , 0, 0, 0 },
+ { "encrypt"          , cipher_encrypt          , 0, 0, 0 },
+ { "decrypt"          , cipher_decrypt          , 0, 0, 0 },
  { 0 }
 };
 
@@ -144,10 +151,16 @@ JSPropertySpec cipher_static_PropertySpec[] = { // *name, tinyid, flags, getter,
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSObject *cipherInitClass( JSContext *cx, JSObject *obj ) {
 
-	return JS_InitClass( cx, obj, NULL, &cipher_class, cipher_construct, 1, cipher_PropertySpec, cipher_FunctionSpec, cipher_static_PropertySpec, NULL );
+// if register_cipher failed but the cipher will not be used, it is acceptable because further check is done on find_cipher call
+
+  return JS_InitClass( cx, obj, NULL, &cipher_class, cipher_construct, 1, cipher_PropertySpec, cipher_FunctionSpec, cipher_static_PropertySpec, NULL );
 }
 
 
 /****************************************************************
+
+CTR ( Counter Mode )
+	http://en.wikipedia.org/wiki/Counter_mode (fr: http://fr.wikipedia.org/wiki/Mode_d%27op%C3%A9ration_%28cryptographie%29 )
+
 
 */
--- jscrypt/crypt.cpp
+++ jscrypt/crypt.cpp
@@ -7,81 +7,289 @@
 
 #include <tomcrypt.h>
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void crypt_Finalize(JSContext *cx, JSObject *obj) {
-}
+#include "cryptError.h"
+
+#include "../common/jshelper.h"
+
+#define MODE_CTR "CTR"
+#define MODE_CFB "CFB"
+
+enum Mode {
+	mode_ctr,
+	mode_cfb
+};
+
+typedef struct PrivateData {
+
+	Mode mode;
+	void *symmetric_XXX;
+};
+
+void crypt_Finalize(JSContext *cx, JSObject *obj);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSClass crypt_class = { "crypt", JSCLASS_HAS_PRIVATE,
-  JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
-  JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, crypt_Finalize
+JSClass crypt_class = { "Crypt", JSCLASS_HAS_PRIVATE,
+	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
+	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, crypt_Finalize
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSBool crypt_construct(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+void crypt_Finalize(JSContext *cx, JSObject *obj) {
 
-	if ( !JS_IsConstructing(cx) ) {
+	PrivateData *privateData = (PrivateData *)JS_GetPrivate( cx, obj );
+	if ( privateData == NULL )
+		return;
 
-		JS_ReportError( cx, "construction is needed" );
-		return JS_FALSE;
+	switch ( privateData->mode ) {
+		case Mode::mode_ctr:
+			ctr_done((symmetric_CTR *)privateData->symmetric_XXX);
+			break;
+		case Mode::mode_cfb:
+			cfb_done((symmetric_CFB *)privateData->symmetric_XXX);
+			break;
 	}
 
-
-
-prng_state prng;
-unsigned char buf[10];
-int err;
-/* start it */
-if ((err = yarrow_start(&prng)) != CRYPT_OK) {
-printf("Start error: %s\n", error_to_string(err));
+	free(privateData->symmetric_XXX);
+	free(privateData);
 }
-/* add entropy */
-if ((err = yarrow_add_entropy((unsigned char*)"hello world", 11, &prng)) != CRYPT_OK) {
-printf("Add_entropy error: %s\n", error_to_string(err));
-}
-/* ready and read */
-if ((err = yarrow_ready(&prng)) != CRYPT_OK) {
-printf("Ready error: %s\n", error_to_string(err));
-}
-printf("Read %lu bytes from yarrow\n", yarrow_read(buf, 10, &prng));
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// mode, cipher, key, IV
+JSBool crypt_construct(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
 
-   register_cipher(&rijndael_enc_desc);
-   register_hash(&sha256_desc);
+	RT_ASSERT( JS_IsConstructing(cx), RT_ERROR_NEED_CONSTRUCTION );
+	RT_ASSERT_ARGC( 4 );
+	RT_ASSERT_CLASS( obj, &crypt_class );
 
+	char *modeName;
+	RT_JSVAL_TO_STRING( argv[0], modeName );
 
-	if (register_prng(&yarrow_desc) == -1) {
-      printf("Error registering sprng PRNG\n");
-      exit(-1);
-   }
+	char *cipherName;
+	RT_JSVAL_TO_STRING( argv[1], cipherName );
 
+	char *key;
+	int keyLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[2], key, keyLength );
+
+	char *IV;
+	int IVLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[3], IV, IVLength );
+
+	PrivateData *privateData = (PrivateData*)malloc( sizeof(PrivateData) );
+	RT_ASSERT( privateData != NULL, RT_ERROR_OUT_OF_MEMORY );
+	
+	int cipherIndex = find_cipher(cipherName);
+	RT_ASSERT_1( cipherIndex != -1, "cipher %s is not registred", cipherName );
+
+	ltc_cipher_descriptor cipher = cipher_descriptor[cipherIndex];
+
+	RT_ASSERT_1( IVLength == cipher.block_length, "IV must have the same size as cipher block length (%d bytes)", cipher.block_length );
+
+	int err;
+	if ( stricmp( modeName, MODE_CTR ) == 0 ) {
+
+		privateData->mode = Mode::mode_ctr;
+		symmetric_CTR *psctr = (symmetric_CTR *)malloc( sizeof(symmetric_CTR) );
+		RT_ASSERT( psctr != NULL, RT_ERROR_OUT_OF_MEMORY );
+		if ((err = ctr_start( cipherIndex, (const unsigned char *)IV, (const unsigned char *)key, keyLength, 0, CTR_COUNTER_LITTLE_ENDIAN, psctr )) != CRYPT_OK)
+			return ThrowCryptError(cx, err); // [TBD] free privateData and psctr
+		privateData->symmetric_XXX = psctr;
+	} else if ( stricmp( modeName, MODE_CFB ) == 0 ) {
+
+		privateData->mode = Mode::mode_cfb;
+		symmetric_CFB *symmetric_XXX = (symmetric_CFB *)malloc( sizeof(symmetric_CFB) );
+		RT_ASSERT( symmetric_XXX != NULL, RT_ERROR_OUT_OF_MEMORY );
+		if ((err = cfb_start( cipherIndex, (const unsigned char *)IV, (const unsigned char *)key, keyLength, 0, symmetric_XXX )) != CRYPT_OK)
+			return ThrowCryptError(cx, err); // [TBD] free privateData and psctr
+		privateData->symmetric_XXX = symmetric_XXX;
+	} else
+		RT_ASSERT_1( false, "unsupported %s mode.", modeName );
+
+	JS_SetPrivate( cx, obj, privateData );
 
 	return JS_TRUE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSBool crypt_myFunction(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+JSBool crypt_encrypt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+
+	RT_ASSERT_ARGC( 1 );
+	RT_ASSERT_CLASS( obj, &crypt_class );
+	PrivateData *privateData = (PrivateData *)JS_GetPrivate( cx, obj );
+	RT_ASSERT( privateData != NULL, RT_ERROR_NOT_INITIALIZED );
+
+	char *pt;
+	int ptLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[0], pt, ptLength );
+
+	char *ct = (char *)JS_malloc( cx, ptLength );
+	RT_ASSERT( ct != NULL, RT_ERROR_OUT_OF_MEMORY );
+
+	int err;
+	switch ( privateData->mode ) {
+
+		case Mode::mode_ctr:
+			if ( (err = ctr_encrypt( (const unsigned char *)pt, (unsigned char *)ct, ptLength, (symmetric_CTR *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		case Mode::mode_cfb:
+			if ( (err = cfb_encrypt( (const unsigned char *)pt, (unsigned char *)ct, ptLength, (symmetric_CFB *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		default:
+			RT_ASSERT( false, "unsupported mode." );
+	}
+
+	JSString *jssCt = JS_NewString( cx, ct, ptLength );
+	RT_ASSERT( jssCt != NULL, "unable to create the cipher string." );
+	*rval = STRING_TO_JSVAL(jssCt);
 
 	return JS_TRUE;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+JSBool crypt_decrypt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+
+	RT_ASSERT_ARGC( 1 );
+	RT_ASSERT_CLASS( obj, &crypt_class );
+	PrivateData *privateData = (PrivateData *)JS_GetPrivate( cx, obj );
+	RT_ASSERT( privateData != NULL, RT_ERROR_NOT_INITIALIZED );
+
+	char *ct;
+	int ctLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( argv[0], ct, ctLength );
+
+	char *pt = (char *)JS_malloc( cx, ctLength );
+	RT_ASSERT( pt != NULL, RT_ERROR_OUT_OF_MEMORY );
+
+	int err;
+	switch ( privateData->mode ) {
+
+		case Mode::mode_ctr:
+			if ( (err = ctr_decrypt( (const unsigned char *)ct, (unsigned char *)pt, ctLength, (symmetric_CTR *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		case Mode::mode_cfb:
+			if ( (err = cfb_decrypt( (const unsigned char *)ct, (unsigned char *)pt, ctLength, (symmetric_CFB *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		default:
+			RT_ASSERT( false, "unsupported mode." );
+	}
+
+	JSString *jssCt = JS_NewString( cx, pt, ctLength );
+	RT_ASSERT( jssCt != NULL, "unable to create the cipher string." );
+	*rval = STRING_TO_JSVAL(jssCt);
+
+	return JS_TRUE;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSFunctionSpec crypt_FunctionSpec[] = { // *name, call, nargs, flags, extra
- { "myFunction"          , crypt_myFunction          , 0, 0, 0 },
+ { "Encrypt"        , crypt_encrypt     , 0, 0, 0 },
+ { "Decrypt"        , crypt_decrypt     , 0, 0, 0 },
  { 0 }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-JSBool crypt_getter_myProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
+JSBool crypt_setter_IV(JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
 
-  return JS_TRUE;
+	RT_ASSERT_CLASS( obj, &crypt_class );
+	PrivateData *privateData = (PrivateData *)JS_GetPrivate( cx, obj );
+	RT_ASSERT( privateData != NULL, RT_ERROR_NOT_INITIALIZED );
+
+	char *IV;
+	int IVLength;
+	RT_JSVAL_TO_STRING_AND_LENGTH( *vp, IV, IVLength );
+
+	int err;
+	switch ( privateData->mode ) {
+		case Mode::mode_ctr:
+			if ( (err=ctr_setiv( (const unsigned char *)IV, IVLength, (symmetric_CTR *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		case Mode::mode_cfb:
+			if ( (err=cfb_setiv( (const unsigned char *)IV, IVLength, (symmetric_CFB *)privateData->symmetric_XXX )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		default:
+			RT_ASSERT( false, "unsupported mode." );
+	}
+
+	return JS_TRUE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+JSBool crypt_getter_IV(JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
+
+	RT_ASSERT_CLASS( obj, &crypt_class );
+	PrivateData *privateData = (PrivateData *)JS_GetPrivate( cx, obj );
+	RT_ASSERT( privateData != NULL, RT_ERROR_NOT_INITIALIZED );
+
+	char *IV;
+	unsigned long IVLength;
+
+	int err;
+	switch ( privateData->mode ) {
+		case Mode::mode_ctr: {
+			symmetric_CTR *psctr = (symmetric_CTR *)privateData->symmetric_XXX;
+			IVLength = psctr->blocklen;
+			IV = (char*)JS_malloc( cx, IVLength );
+			RT_ASSERT( IV != NULL, RT_ERROR_OUT_OF_MEMORY );
+			if ( (err=ctr_getiv( (unsigned char *)IV, &IVLength, psctr )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		}
+		case Mode::mode_cfb: {
+			symmetric_CFB *psctr = (symmetric_CFB *)privateData->symmetric_XXX;
+			IVLength = psctr->blocklen;
+			IV = (char*)JS_malloc( cx, IVLength );
+			RT_ASSERT( IV != NULL, RT_ERROR_OUT_OF_MEMORY );
+			if ( (err=cfb_getiv( (unsigned char *)IV, &IVLength, psctr )) != CRYPT_OK )
+				return ThrowCryptError(cx, err);
+			break;
+		}
+		default:
+			RT_ASSERT( false, "unsupported mode." );
+	}
+
+	JSString *jssIV = JS_NewString( cx, IV, IVLength );
+	RT_ASSERT( jssIV != NULL, "unable to create the IV string." );
+	*vp = STRING_TO_JSVAL(jssIV);
+
+	return JS_TRUE;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSPropertySpec crypt_PropertySpec[] = { // *name, tinyid, flags, getter, setter
-	{ "myProperty"            , 0, JSPROP_PERMANENT|JSPROP_READONLY, crypt_getter_myProperty       , NULL },
+	{ "IV"            , 0, JSPROP_PERMANENT, crypt_getter_IV, crypt_setter_IV },
   { 0 }
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+JSBool crypt_static_blockLength(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+
+	RT_ASSERT_ARGC( 1 );
+	char *cipherName;
+	RT_JSVAL_TO_STRING( argv[0], cipherName );
+
+	int cipherIndex = find_cipher(cipherName);
+	RT_ASSERT_1( cipherIndex != -1, "cipher %s is not registred", cipherName );
+
+	*rval = INT_TO_JSVAL(cipher_descriptor[cipherIndex].block_length);
+
+	return JS_TRUE;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+JSFunctionSpec crypt_static_FunctionSpec[] = { // *name, call, nargs, flags, extra
+	{ "BlockLength"        , crypt_static_blockLength  , 0, 0, 0 },
+	{ 0 }
+};
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSBool crypt_static_getter_myStatic(JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
@@ -91,14 +299,74 @@ JSBool crypt_static_getter_myStatic(JSContext *cx, JSObject *obj, jsval id, jsva
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSPropertySpec crypt_static_PropertySpec[] = { // *name, tinyid, flags, getter, setter
-	{ "myStatic"                , 0, JSPROP_PERMANENT|JSPROP_READONLY, crypt_static_getter_myStatic         , NULL },
+//	{ "myStatic"                , 0, JSPROP_PERMANENT|JSPROP_READONLY, crypt_static_getter_myStatic         , NULL },
   { 0 }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 JSObject *cryptInitClass( JSContext *cx, JSObject *obj ) {
 
-	return JS_InitClass( cx, obj, NULL, &crypt_class, crypt_construct, 1, crypt_PropertySpec, crypt_FunctionSpec, crypt_static_PropertySpec, NULL );
+
+#ifdef RIJNDAEL
+  register_cipher (&aes_desc);
+#endif
+#ifdef BLOWFISH
+  register_cipher (&blowfish_desc);
+#endif
+#ifdef XTEA
+  register_cipher (&xtea_desc);
+#endif
+#ifdef RC5
+  register_cipher (&rc5_desc);
+#endif
+#ifdef RC6
+  register_cipher (&rc6_desc);
+#endif
+#ifdef SAFERP
+  register_cipher (&saferp_desc);
+#endif
+#ifdef TWOFISH
+  register_cipher (&twofish_desc);
+#endif
+#ifdef SAFER
+  register_cipher (&safer_k64_desc);
+  register_cipher (&safer_sk64_desc);
+  register_cipher (&safer_k128_desc);
+  register_cipher (&safer_sk128_desc);
+#endif
+#ifdef RC2
+  register_cipher (&rc2_desc);
+#endif
+#ifdef DES
+  register_cipher (&des_desc);
+  register_cipher (&des3_desc);
+#endif
+#ifdef CAST5
+  register_cipher (&cast5_desc);
+#endif
+#ifdef NOEKEON
+  register_cipher (&noekeon_desc);
+#endif
+#ifdef SKIPJACK
+  register_cipher (&skipjack_desc);
+#endif
+#ifdef KHAZAD
+  register_cipher (&khazad_desc);
+#endif
+#ifdef ANUBIS
+  register_cipher (&anubis_desc);
+#endif
+
+/*
+	// you must register a cipher before you use it
+	if ( register_cipher(&blowfish_desc) == -1 ) {
+
+		JS_ReportError( cx, "Unable to register Blowfish cipher." );
+		return JS_FALSE;
+	}
+*/
+
+	return JS_InitClass( cx, obj, NULL, &crypt_class, crypt_construct, 0, crypt_PropertySpec, crypt_FunctionSpec, crypt_static_PropertySpec, crypt_static_FunctionSpec );
 }
 
 
