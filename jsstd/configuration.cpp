ec1f443aeec8320f7fee4f60beadd43d27572184
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-09-28 22:15:02 +0000

    - remove CreateConfigurationObject()

--- jsstd/configuration.cpp
+++ jsstd/configuration.cpp
@@ -6,29 +6,6 @@ JSClass configuration_class = { "Configuration", JSCLASS_HAS_PRIVATE,
 	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
 	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub
 };
-/*
-Configuration *CreateConfigurationObject( JSContext *cx ) {
-
-	JSObject *global = JS_GetGlobalObject(cx);
-	if ( global == NULL )
-		return NULL;
-
-	Configuration *configuration = (Configuration *)JS_malloc( cx, sizeof(Configuration) );
-	if ( configuration == NULL )
-		return NULL;
-	
-	JSObject *configObject = JS_ConstructObject( cx, &configuration_class, NULL, NULL );
-	if ( configObject == NULL )
-		return NULL;
-
-	JS_SetPrivate( cx, configObject, configuration );
-
-	jsval jsvalConfig = OBJECT_TO_JSVAL(configObject);
-	JS_SetProperty( cx, global, "configuration", &jsvalConfig );
-
-	return configuration;
-}
-*/
 
 Configuration *GetConfiguration( JSContext *cx ) {
 
--- jsstd/configuration.h
+++ jsstd/configuration.h
@@ -14,6 +14,5 @@ struct Configuration {
 
 extern JSClass configuration_class;
 
-//Configuration *CreateConfigurationObject( JSContext *cx );
 Configuration *GetConfiguration( JSContext *cx );
 
