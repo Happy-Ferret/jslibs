4fdd187c4774053b62c0545e6122f1f779e25605
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2006-10-25 22:12:13 +0000

    - add SpiderMonkeyTools static library

---
+++ smtools/smtools.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "smtools.h"
+
+JSBool IntArrayToVector( JSContext *cx, int count, const jsval *vp, int *vector ) {
+
+	JSObject *jsArray;
+	RT_SAFE( JS_ValueToObject(cx, *vp, &jsArray) );
+	RT_UNSAFE( jsArray = JSVAL_TO_OBJECT(*vp) );
+	RT_ASSERT( JS_IsArrayObject(cx,jsArray), "value must be an array." );
+	jsval value;
+	int32 i32;
+	for (int i=0; i<count; ++i) {
+
+		JS_GetElement(cx, jsArray, i, &value );
+		RT_SAFE( jsdouble d; JS_ValueToNumber(cx, value, &d); i32=d; );
+		RT_UNSAFE( i32 = JSVAL_TO_INT(value) );
+		vector[i] = i32;
+	}
+	return JS_TRUE;
+}
+
+JSBool IntVectorToArray( JSContext *cx, int count, const int *vector, jsval *vp ) {
+
+	JSObject *jsArray = JS_NewArrayObject(cx, 0, NULL);
+	*vp = OBJECT_TO_JSVAL(jsArray);
+	jsval value;
+	int32 i32;
+	for (int i=0; i<count; ++i) {
+
+		RT_SAFE( JS_NewNumberValue(cx, vector[i], &value) );
+		RT_UNSAFE( value = INT_TO_JSVAL(vector[i]) );
+		JS_SetElement(cx, jsArray, i, &value);
+	}
+	return JS_TRUE;
+}
---
+++ smtools/smtools.h
@@ -0,0 +1,5 @@
+JSBool IntArrayToVector( JSContext *cx, int count, const jsval *vp, int *vector );
+JSBool IntVectorToArray( JSContext *cx, int count, const int *vector, jsval *vp );
+
+//JSBool ArrayToVector( JSContext *cx, int dim, jsval *vp, real *vector );
+//JSBool VectorToArray( JSContext *cx, int dim, const real *vector, jsval *vp );
