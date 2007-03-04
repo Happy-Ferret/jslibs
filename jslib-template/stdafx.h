85c5fe554fb93fabd984daa8282d1e10cfc2061d
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-03-04 15:10:14 +0000

    - update template project

--- jslib-template/stdafx.h
+++ jslib-template/stdafx.h
@@ -12,6 +12,7 @@
 
 #define XP_WIN
 #include <jsapi.h>
-#include "../common/jshelper.h"
-#include "../common/jsclass.h"
+#include "../common/jsNames.h"
+#include "../common/jsHelper.h"
+#include "../common/jsClass.h"
 
