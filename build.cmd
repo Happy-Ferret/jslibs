cb82f28d3533362816c8f82f1ad8a6e7f4233811
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-08-01 16:41:38 +0000

    - update global build script

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
-set prevDir=%CD%
-set prevPath=%PATH%
-
 set visualStudioPath=C:\Program Files\Microsoft Visual Studio 8
 set platformSDKPath=C:\Program Files\Microsoft Platform SDK
 
 rem --------------------------------------------------------------------------
+
+set prevDir=%CD%
+set prevPath=%PATH%
 
 call "%platformSDKPath%\SetEnv.Cmd" /XP32 /RETAIL
 call "%visualStudioPath%\VC\vcvarsall.bat" x86
@@ -18,13 +18,17 @@ for /D %%f in (js*) do (
 		echo building %%g ...
 		rem VCBUILD Options: http://msdn2.microsoft.com/en-us/library/cz553aa1(VS.80).aspx
 		"%visualStudioPath%\VC\vcpackages\vcbuild" /useenv /rebuild %%g "RELEASE|WIN32" >> build.log
-		if ERRORLEVEL 1 echo ... failed.
-rem		if ERRORLEVEL 1 goto error
-		pushd %%~dg%%~pgrelease
-		copy *.dll ..\..
-		copy *.exe ..\..
-		popd
+		if ERRORLEVEL 1 (
 		
+			echo ... failed.
+rem		if ERRORLEVEL 1 goto error
+		) ELSE (
+		
+			pushd %%~dg%%~pgrelease
+			copy *.dll ..\..
+			copy *.exe ..\..
+			popd
+		)
 	)
 )
 
