e3803e8f83ed5e361383a8ba9541136f1690db34
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-08-06 22:03:59 +0000

    - add comments for tests

--- js/build_msdev8_DBG.bat
+++ js/build_msdev8_DBG.bat
@@ -8,13 +8,20 @@ call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
 
 cd .\src
 
-rem /MT  : Multithreaded static
-rem /MTd : Multithreaded static debug
-rem /MD  : Multithreaded DLL
-rem /MDd : Multithreaded DLL debug
+	rem /MT  : Multithreaded static
+	rem /MTd : Multithreaded static debug
+	rem /MD  : Multithreaded DLL
+	rem /MDd : Multithreaded DLL debug
 set XCFLAGS=/D_CRT_SECURE_NO_DEPRECATE=1 /MDd
 
+	rem set OTHER_LIBS=../../nspr/win32/dist/lib/nspr4.lib /NODEFAULTLIB:libnspr4.lib
+
+	rem Linker Options: http://msdn2.microsoft.com/en-us/library/y0zzbyt4(VS.80).aspx
+	rem TEMPORARY ( may crash with Date object ) :
+	rem set OTHER_LIBS=/FORCE:UNRESOLVED
+
 make -f Makefile.ref clean all   JS_THREADSAFE=1 JS_DIST=../../nspr
+	rem JS_DIST=../../nspr/win32/dist
 
 set PATH=%prevPath%
 cd /D %prevDir%
