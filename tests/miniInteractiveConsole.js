ef5ae520f69934ab47688c97f7509d7b6cfbdb2d
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-08-28 20:56:23 +0000

    - better interactive console

--- tests/miniInteractiveConsole.js
+++ tests/miniInteractiveConsole.js
@@ -1,6 +1,28 @@
 LoadModule('jsstd');
-LoadModule('jsnspr');
+LoadModule('jsio');
+
+global.__defineGetter__('quit', Halt);
+global.__defineGetter__('exit', Halt);
+global.__defineGetter__('bye', Halt);
 
 for (;;) {
-	Print( eval( File.stdin.ReadAll() ), '\n' );
+	
+	Print('js> ');
+
+	var code = '';
+	do {
+	
+		code += File.stdin.Read();
+	} while( !IsStatementValid( code ) );
+
+	try {
+
+		var res = eval( code );
+	} catch(ex) {
+
+		Print( ex, '\n' );
+	}
+
+	if ( res != undefined )
+		Print( res, '\n' );
 }
