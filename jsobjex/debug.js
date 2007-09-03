5e92f76559cff468c3793dd166d197d0ff059bb9
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-09-03 16:49:45 +0000

    - add a new test

--- jsobjex/debug.js
+++ jsobjex/debug.js
@@ -10,3 +10,15 @@ var obj = new ObjEx( addCallback, undefined, undefined, undefined, null );
 
 obj.foo = 123;
 obj.foo = 456;
+
+
+function Error(text) {
+	throw(text);
+}
+
+
+var api = new ObjEx( undefined,undefined,undefined, function(name, value) this[name] ? Error('API Already defined') : value );
+
+api.toto = function() Print('ok\n');
+api.toto();
+api.toto = function() Print('ok2\n');
