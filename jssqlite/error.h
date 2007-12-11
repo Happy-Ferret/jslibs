dbef6db0547799f3e67ce313d2e3252c8ab1598b
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2007-12-11 12:40:59 +0000

    - working on Linux compilation.

--- jssqlite/error.h
+++ jssqlite/error.h
@@ -12,7 +12,7 @@
  * License.
  * ***** END LICENSE BLOCK ***** */
 
-DECLARE_CLASS( SqliteError );
+DECLARE_CLASS( SqliteError )
 
 #define SLOT_SQLITE_ERROR_CODE 0
 #define SLOT_SQLITE_ERROR_TEXT 1
