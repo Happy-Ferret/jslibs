b5123352ef82f3f3083d0cd91a9ebaa82a1fdeb2
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-01-28 19:14:42 +0000

    - add a batch file to set a usable environment to run jshost

---
+++ setenv.cmd
@@ -0,0 +1,2 @@
+set BLD=release
+set PATH=%CD%\js\src\WINNT5.1_OPT.OBJ;%CD%\jshost\%BLD%;%CD%\jswinhost\%BLD%;%CD%\jsstd\%BLD%;%CD%\jsio\%BLD%;%CD%\nspr\win32\dist\lib;%CD%\jssqlite\%BLD%;%CD%\jsobjex\%BLD%;%CD%\jsz\%BLD%;%CD%\jscrypt\%BLD%;%CD%\jswinshell\%BLD%;%CD%\jstest\%BLD%;%PATH%
