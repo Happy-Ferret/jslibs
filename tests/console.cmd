2931cbd4dfd7ca2e1b2170c72c50701636441746
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-02-13 23:29:21 +0000

    - use setenv.cmd

--- tests/console.cmd
+++ tests/console.cmd
@@ -1,7 +1,4 @@
-cd ..
-
-set BLD=release
-set PATH=%CD%\js\src\WINNT5.1_OPT.OBJ;%CD%\jshost\%BLD%;%CD%\jswinhost\%BLD%;%CD%\jsstd\%BLD%;%CD%\jsio\%BLD%;%CD%\nspr\win32\dist\lib;%CD%\jssqlite\%BLD%;%CD%\jsobjex\%BLD%;%CD%\jsz\%BLD%;%CD%\jscrypt\%BLD%;%CD%\jswinshell\%BLD%;%CD%\jstest\%BLD%;%PATH%
-
-cd tests
-cmd /k 
+pushd ..
+call setenv.cmd
+popd
+cmd /k
