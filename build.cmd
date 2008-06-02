aafeaea1b448a1e48dbbaa2f5b7ebd75bd482cf2
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-06-02 21:20:01 +0000

    - add DirectX directories to command-line build.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 set visualStudioPath=C:\Program Files\Microsoft Visual Studio 8
 set platformSDKPath=C:\Program Files\Microsoft Platform SDK
+set DirectXPath=C:\Program Files\Microsoft DirectX SDK (March 2008)
 
 rem --------------------------------------------------------------------------
 @echo off
@@ -9,10 +10,18 @@ IF "%BUILD%"=="" set BUILD=release
 set prevDir=%CD%
 set prevPath=%PATH%
 
+Set Lib=%DirectXPath%\Lib\x86;%Lib%
+Set Include=%DirectXPath%\Include;%Include%
+
 call "%platformSDKPath%\SetEnv.Cmd" /XP32 /RETAIL
+
 call "%visualStudioPath%\VC\vcvarsall.bat" x86
 
-del build.log
+
+
+date /T > build.log
+set > build.log
+
 
 md .\%BUILD%
 
@@ -27,6 +36,8 @@ for /D %%f in (js*) do (
 	for %%g in (%%f\*.sln) do (
 		
 		echo building %%g ...
+		echo ******************************************************************************* >> build.log
+		echo ******************************************************************************* >> build.log
 		rem VCBUILD Options: http://msdn2.microsoft.com/en-us/library/cz553aa1(VS.80).aspx
 		"%visualStudioPath%\VC\vcpackages\vcbuild" /useenv /rebuild %%g "%BUILD%|WIN32" >> build.log
 		if ERRORLEVEL 1 (
