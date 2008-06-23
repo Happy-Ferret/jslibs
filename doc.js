00c183df2ce34021ed424289c8478286dad40730
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-06-23 17:02:53 +0000

    - finish doc extractor script.

--- doc.js
+++ doc.js
@@ -1,7 +1,8 @@
 LoadModule('jsstd');
 LoadModule('jsio');
 
-function Map() ({ __proto__:null });
+//function Map() ({ __proto__:null });
+function Map() ({});
 
 function RecursiveDir(path, callback) {
 	
@@ -26,13 +27,14 @@ function RecursiveDir(path, callback) {
 }
 
 
-function CreateItemList() {
+function CreateDocItemList() {
 
 	var docExpr = /\/\*\*doc(?: (.*)\r?\n)?((.*\r?\n)*?)\*\*\//g;
 	var hidden = /\/\./;
 	var sourceCode = /\.(h|cpp|c)$/;
 
 	var itemList = [];
+	var index = 0;
 
 	RecursiveDir( './src', function(file) {
 
@@ -45,18 +47,18 @@ function CreateItemList() {
 
 				var item = Map();
 				itemList.push(item);
-
+				
+				item.index = index++;
 				item.filePath = file.name;
 				item.path = file.name.substr(0, file.name.lastIndexOf('/'));
 				item.lastDir = item.path.substr(item.path.lastIndexOf('/')+1);
 				item.fileName = file.name.substr(file.name.lastIndexOf('/')+1);
-				item.wikiText = res[2];
+				item.text = res[2];
 				item.source = source;
-				item.sourceIndex = docExpr.lastIndex - item.wikiText.length;
+				item.sourceIndex = docExpr.lastIndex - item.text.length;
 				
-//				var attr = Map();
-				var attr = {};
-				item.attributs = attr;
+				var attr = Map();
+				item.attr = attr;
 				
 				if ( res[1] )
 					for each ( var a in res[1].split(' ') ) {
@@ -72,47 +74,94 @@ function CreateItemList() {
 }
 
 
-function docBuilder(itemList) {
+function MakeGroups( list, groupFunction ) {
 
-	itemList.sort(function(a,b) (b.attributs.o||0) - (a.attributs.o||0)); // o||0 manages: x > undefined and undefined > x
-	
-	
-	var doc = <doc/>;
+	var group, grouped = {};
+	for each ( var item in list ) {
 
-	var wikiFile;
-	for ( var [i,item] in Iterator(itemList) ) {
-		
-		if ( item.attributs.f )
-			wikiFile = item.attributs.f;
-
-		doc[wikiFile]
-
-		var currentFile = doc[wikiFile] || (doc[wikiFile] = []);
-		
-		if ( item.attributs.g )
-		
-		currentFile
-	
-		
-		Print( item.path,' ', item.attributs.toSource(), '\n' );
-	
-
+		group = groupFunction(item, group);
+		(grouped[group] || (grouped[group] = [])).push(item);
 	}
+	return [ g for each ( g in grouped ) ];
 }
 
-docBuilder(CreateItemList());
+
+function FlattenGroup( groupList ) {
+
+	var flat = [];
+	for each ( var group in groupList )
+		flat = flat.concat(group);
+	return flat;
+}
+
+
+var itemList = CreateDocItemList();
+
+// make module and file group
+var moduleList = MakeGroups( itemList, function(item) item.lastDir );
+for ( var module in moduleList )
+	moduleList[module] = MakeGroups( moduleList[module], function(item) item.filePath );
+
+// order doc items by group ( g:* )
+for each ( var module in moduleList )
+	for ( var filePath in module )
+		module[filePath] = FlattenGroup( MakeGroups( module[filePath], function(item, prevGroup) item.attr.g || prevGroup ) );
+
+/*
+// move to the top of its module the file that contains t:header	
+for each ( var module in moduleList )
+	module.sort(function(a,b) {
+		for each ( var item in a )
+			if ( item.attr.t == 'header' )
+				return -1;
+		return 1;
+	} );
+*/
+
+
+// sort files in each module
+for each ( var module in moduleList )
+
+	module.sort(function(a,b) {
+		for each ( var item in a )
+			if ( item.fileName == 'static.cpp' )
+				return -1;
+		return 1;
+	} );
 
 
 
-/**doc #HideProperties
- * void *ASSERT*( expression [, failureMessage ] )
-  If the argument expression compares equal to zero, the failureMessage is written to the standard error device and the program stops its execution.
-**/
+
+// move to the top of its module the docitem that contains t:header	
+for each ( var module in moduleList )
+	for each ( var file in module )
+		for ( var i in file ) {
+			if ( file[i].attr.t == 'header' ) {
+
+				module.unshift([file[i]]);
+				file.splice(i,1);
+				break;
+			}
+			if ( file[i].attr.t == 'footer' ) {
+
+				module.push([file[i]]);
+				file.splice(i,1);
+				break;
+			}
+		}
 
 
-/**doc #example #HideProperties 
-  var foo = ['a', 'b', 'c'];
-  ASSERT( i >= 0 || i < 3, 'Invalid value.' );
-  Print( foo[i] );
-**/
+// write the doc
+for each ( var module in moduleList ) {
 
+	var moduleName = module[0][0].lastDir;
+	var f = new File( moduleName+'_.wiki' );
+	f.Open('w');
+	for each ( var file in module )
+		for each ( var item in file )
+			f.Write( item.text ); 
+	f.Close();
+}
+
+
+Print('Done.');
