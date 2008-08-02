d7483b8ba2bdca8643dc6418f343955ad8ddb3d5
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-08-02 13:11:33 +0000

    - fix

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,9 @@
-[ -z $BUILD ] && set BUILD=opt
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/$BUILD
-export PATH=$PATH:$PWD/$BUILD
+#!/bin/bash
+
+[ -z "$BUILD" ] && BUILD=opt
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/$BUILD
+PATH=$PATH:$PWD/$BUILD
+
+export LD_LIBRARY_PATH
+export PATH
