c6b0002819ad4d03b73caca629613e68610d86d2
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-08-24 21:50:27 +0000

    - update jslibs env.

--- setenv.cmd
+++ setenv.cmd
@@ -5,6 +5,6 @@ IF "%BUILD%"=="" set BUILD=release
 IF "%BUILD%"=="release" set PATH=%CD%\libs\js\src\WINNT5.1_OPT.OBJ;%PATH%
 IF "%BUILD%"=="debug" set PATH=%CD%\libs\js\src\WINNT5.1_DBG.OBJ;%PATH%
 
-set PATH=%PATH%;%CD%\src\jshost\%BUILD%;%CD%\src\jswinhost\%BUILD%;%CD%\src\jslang\%BUILD%;%CD%\src\jsstd\%BUILD%;%CD%\src\jsio\%BUILD%;%CD%\libs\nspr\win32\dist\lib;%CD%\src\jssqlite\%BUILD%;%CD%\src\jsobjex\%BUILD%;%CD%\src\jsz\%BUILD%;%CD%\src\jscrypt\%BUILD%;%CD%\src\jswinshell\%BUILD%;%CD%\src\jsdebug\%BUILD%;%CD%\src\jsimage\%BUILD%;%CD%\src\jssound\%BUILD%;%CD%\src\jsfont\%BUILD%;%CD%\src\jsprotex\%BUILD%
+set PATH=%PATH%;%CD%\src\jshost\%BUILD%;%CD%\src\jswinhost\%BUILD%;%CD%\src\jslang\%BUILD%;%CD%\src\jsstd\%BUILD%;%CD%\src\jsio\%BUILD%;%CD%\libs\nspr\win32\dist\lib;%CD%\src\jssqlite\%BUILD%;%CD%\src\jsobjex\%BUILD%;%CD%\src\jsz\%BUILD%;%CD%\src\jscrypt\%BUILD%;%CD%\src\jswinshell\%BUILD%;%CD%\src\jsdebug\%BUILD%;%CD%\src\jsimage\%BUILD%;%CD%\src\jssound\%BUILD%;%CD%\src\jsaudio\%BUILD%;%CD%\src\jsfont\%BUILD%;%CD%\src\jsprotex\%BUILD%;%CD%\libs\openal\src\OpenAL-Windows\OpenAL32\Release
 
 echo jslibs %BUILD% environment set.
