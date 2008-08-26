92d17c34afb185cecedd5fb5404bdf27a5ea41a3
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-08-26 21:56:26 +0000

    - update env file

--- setenv.cmd
+++ setenv.cmd
@@ -5,6 +5,26 @@ IF "%BUILD%"=="" set BUILD=release
 IF "%BUILD%"=="release" set PATH=%CD%\libs\js\src\WINNT5.1_OPT.OBJ;%PATH%
 IF "%BUILD%"=="debug" set PATH=%CD%\libs\js\src\WINNT5.1_DBG.OBJ;%PATH%
 
-set PATH=%PATH%;%CD%\src\jshost\%BUILD%;%CD%\src\jswinhost\%BUILD%;%CD%\src\jslang\%BUILD%;%CD%\src\jsstd\%BUILD%;%CD%\src\jsio\%BUILD%;%CD%\libs\nspr\win32\dist\lib;%CD%\src\jssqlite\%BUILD%;%CD%\src\jsobjex\%BUILD%;%CD%\src\jsz\%BUILD%;%CD%\src\jscrypt\%BUILD%;%CD%\src\jswinshell\%BUILD%;%CD%\src\jsdebug\%BUILD%;%CD%\src\jsimage\%BUILD%;%CD%\src\jssound\%BUILD%;%CD%\src\jsaudio\%BUILD%;%CD%\src\jsfont\%BUILD%;%CD%\src\jsprotex\%BUILD%;%CD%\libs\openal\src\OpenAL-Windows\OpenAL32\Release
+
+set PATH=%PATH%;%CD%\src\jshost\%BUILD%;
+set PATH=%PATH%;%CD%\src\jswinhost\%BUILD%;
+set PATH=%PATH%;%CD%\src\jslang\%BUILD%;
+
+set PATH=%PATH%;%CD%\src\jsstd\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsio\%BUILD%;
+set PATH=%PATH%;%CD%\libs\nspr\win32\dist\lib;
+set PATH=%PATH%;%CD%\src\jssqlite\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsobjex\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsz\%BUILD%;
+set PATH=%PATH%;%CD%\src\jscrypt\%BUILD%;
+set PATH=%PATH%;%CD%\src\jswinshell\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsdebug\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsimage\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsgraphics\%BUILD%;
+set PATH=%PATH%;%CD%\src\jssound\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsaudio\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsfont\%BUILD%;
+set PATH=%PATH%;%CD%\src\jsprotex\%BUILD%;
+set PATH=%PATH%;%CD%\libs\openal\src\OpenAL-Windows\OpenAL32\Release
 
 echo jslibs %BUILD% environment set.
