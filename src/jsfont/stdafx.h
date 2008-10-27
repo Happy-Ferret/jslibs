c3b7e397b7eaf8c7739c90cd5cb9e00ed84ef3ea
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2008-10-27 17:51:51 +0000

    - fix Linux compilation.

--- src/jsfont/stdafx.h
+++ src/jsfont/stdafx.h
@@ -5,11 +5,6 @@
 
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-// TODO: reference additional headers your program requires here
-
-#include <windows.h>
-
 #include "../common/platform.h"
 
 #include <jsapi.h>
