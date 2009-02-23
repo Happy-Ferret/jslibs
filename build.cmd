9d3ead536e33b41fce0a23258038e1a55df87531
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-02-23 18:50:15 +0000

    - minor fix in path usage.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ set MOZ_BUILD_SCRIPT=C:\TOOLS\mozilla-build\start-msvc8.bat
 
 rem ==========================================================================
 for %%A in ("%CD%") do set _CD_SHORT=%%~sfA
-echo cd %_CD_SHORT%;sh build.sh | call %MOZ_BUILD_SCRIPT%
+echo cd "%_CD_SHORT%";sh build.sh | call %MOZ_BUILD_SCRIPT%
 pause
