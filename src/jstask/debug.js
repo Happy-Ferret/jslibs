0b1b974e5b5eb1517666344e8c9a45fe92483e77
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-06 21:16:32 +0000

    - minor modifications.

--- src/jstask/debug.js
+++ src/jstask/debug.js
@@ -1,22 +1,12 @@
 LoadModule('jsstd');
 LoadModule('jsio');
 LoadModule('jstask');
+
+
+var myTask = new Task(function(){});
+
+myTask.Request(undefined);
+myTask.Response();
 	
-	var res = new File('debug.js').content;
-
-	function MyFileTask(filename, i) {
-		
-		if ( !i )
-			LoadModule('jsio');
-		var res = new File(filename).content;
-		return res;
-	}
-
-	var myTask = new Task(MyFileTask);
-
-	var filename = 'qwrqwdrqwd'+'.tmp';
-	new File(filename).content = 'XXX'+filename;
-	myTask.Request(filename);
-	Sleep(10);
-	var res = myTask.Response();
-	Print( res == 'XXX'+filename, '\n' );
+	
+	
