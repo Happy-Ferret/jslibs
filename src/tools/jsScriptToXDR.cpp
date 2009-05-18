2502eccc6e3594e1cc28519301c32acf7dae2aca
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-18 20:29:40 +0000

    - create a simple binary file to convert a script into a XDRed script.

---
+++ src/tools/jsScriptToXDR.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#define XP_WIN
+#include <jsapi.h>
+#include <jsxdrapi.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <io.h>
+
+
+int main(int argc, char* argv[]) {
+
+	if ( argc < 2 )
+		return EXIT_FAILURE;
+
+	JSRuntime *rt = JS_NewRuntime(8192);
+	JSContext *cx = JS_NewContext(rt, 8192);
+	
+	JSScript *script = JS_CompileFile(cx, JS_GetGlobalObject(cx), argv[1]);
+	if ( !script ) {
+		
+		printf( "unable to compile %s\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	int file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_BINARY | O_SEQUENTIAL, 00700);
+	if ( file <= 0 ) {
+
+		printf( "unable to create %s\n", argv[2]);
+		return EXIT_FAILURE;
+	}
+
+	JSXDRState *xdr = JS_XDRNewMem(cx, JSXDR_ENCODE);
+	JS_XDRScript(xdr, &script);
+
+	uint32 length;
+	void *buf = JS_XDRMemGetData(xdr, &length);
+	write(file, buf, length);
+	close(file);
+
+	JS_XDRDestroy(xdr);
+
+	JS_DestroyContext(cx);
+	JS_DestroyRuntime(rt);
+	JS_ShutDown();
+
+	return EXIT_SUCCESS;
+}
