d6a925a078e0bc4957152d62fc83487eee7dc423
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-21 20:05:23 +0000

    - dist enhancement.

--- dist/run console.cmd
+++ dist/run console.cmd
@@ -1,6 +1,10 @@
 @echo off
 set PATH=%PATH%;%CD%\bin
 cd examples
-echo Try: jshost helloworld.js
+echo available tests:
+echo ---------------
+dir /B *.js
+echo.
+echo Try: jshost ^<test name^>.js
 echo.
 cmd /k
