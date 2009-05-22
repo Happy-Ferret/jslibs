142d698eb9983d794960be5d9d6114be4418e3d6
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-22 09:12:34 +0000

    - update example

--- tests/helloworld.js
+++ tests/helloworld.js
@@ -1,15 +1,39 @@
+// he l
 LoadModule('jsstd');
 LoadModule('jsio');
 
-function PrintWorld() {
 
-	Print('World');
+function Cap(s) {
+	
+	return s[0].toUpperCase() + s.substr(1);
 }
 
 
-Print('Hello ');
-PrintWorld();
-Print('\n');
+function CreateWorld() {
+
+	var w = global.arguments[0].substr(-8,5);
+	return Cap(w);
+}
+
+
+var regexp = /\/\/ (.*)/;
+var file = new File( arguments[0] );
+var result = regexp( file.content );
+
+var str = result[1];
+
+str = str.split('');
+str[0] = str[0].toUpperCase();
+str[2] = 'l';
+str = str.join('');
+
+str += String.fromCharCode(111) + ' ';
+
+str += CreateWorld();
+
+
+
+Print(str,'!\n');
 
 Print('Press Enter key to continue...');
 
