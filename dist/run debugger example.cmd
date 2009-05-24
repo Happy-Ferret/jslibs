faeb267f39bc53c359223c03116269786d5e608d
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-24 13:55:54 +0000

    - Enhance firefox detection for the debugger example.

--- dist/run debugger example.cmd
+++ dist/run debugger example.cmd
@@ -1,4 +1,10 @@
 @echo off
 set PATH=%PATH%;%CD%\bin
-start bin/debugger.xul
+set firefox="%ProgramFiles%\Mozilla Firefox\firefox.exe"
+if not exist %firefox% (
+ echo Unable to run firefox at %firefox%.
+ pause
+ exit
+)
+start %firefox% bin\debugger.xul
 jshost bin/debugger.js examples/helloworld.js
