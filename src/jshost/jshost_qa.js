13106cd0c12be58fab1c0ec04ff880b29dc92b54
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-05-24 15:24:20 +0000

    - enhance QA test.

--- src/jshost/jshost_qa.js
+++ src/jshost/jshost_qa.js
@@ -10,13 +10,14 @@
 		QA.ASSERT( scripthostpath.length > 0, true, 'script host path' );
 		QA.ASSERT( scripthostname.substr(0, 6), 'jshost', 'script host name' );
 		QA.ASSERT_TYPE( _configuration, 'object', 'has configuration object' );
-		QA.ASSERT_TYPE( global, 'object', 'has global object' );
+		QA.ASSERT_TYPE( global, 'object', 'has "global" property' );
+		QA.ASSERT( global, Object.__parent__, 'global points to the right global object' );
 		
 		var mod = LoadModule('jsstd');
 		var mod1 = LoadModule('jsstd');
 		QA.ASSERT( mod, mod1, 'LoadModule' );
 		QA.ASSERT( 'unsafeMode' in _configuration, true, 'unsafe mode is present' );
-		QA.ASSERT( global.arguments[0], 'qa.js', 'javascript program name' );
+		QA.ASSERT( global.arguments[0].substr(-5), 'qa.js', 'javascript program name' );
 
 
 /// eval function [ftrm]
