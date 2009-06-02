af78814131423380e0fc1023b70e3e3d100c85d1
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-06-02 21:10:50 +0000

    - update Blender exporter.

--- src/jstrimesh/blender_json_export.py
+++ src/jstrimesh/blender_json_export.py
@@ -50,48 +50,42 @@ def exportToFile(filename):
 		Write( text + '\n' )
 
 
-	Write( '({' );
+	Write( '{' );
 	for o in selectedObjectList:
 	
+		# type: 'Armature', 'Camera', 'Curve', 'Lamp', 'Lattice', 'MBall', 'Mesh', 'Surf', 'Empty', 'Wave' (deprecated) or 'unknown' in exceptional cases. 
 		if o.getType() == "Empty":
-			Write( o.name+':{type:"'+o.getType()+'",parent:"'+str(o.parent)+'",position:[%s,%s,%s]' % o.loc + '},' );
+			Write( '"'+o.name+'":{"type":"'+o.getType()+'","parent":"'+str(o.parent)+'","position":[%s,%s,%s]' % o.loc + '},' );
 		
 		if o.getType() == "Mesh":
-			Write( o.name+':{type:"'+o.getType()+'",parent:"'+str(o.parent)+'",position:[%s,%s,%s]' % o.loc );
+			Write( '"'+o.name+'":{"type":"'+o.getType()+'","parent":"'+str(o.parent)+'","position":[%s,%s,%s]' % o.loc + ',' );
 
+			Write( '"mass":%s,' % o.rbMass ); # try: getSBMass()
 
-			BB = o.getBoundBox()
-			objectCenter=[BB[0][n]+(BB[-2][n]-BB[0][n])/2.0 for n in [0,1,2]] 
+			BB = o.getBoundBox(); # list of 8 (x,y,z) float coordinate vectors (WRAPPED DATA)
+			objectCenter = [ BB[0][n]+(BB[-2][n]-BB[0][n])/2.0 for n in [0,1,2] ]
 
-			Write( ',center:[%s,%s,%s]' % (objectCenter[0],objectCenter[1],objectCenter[2]) );
-			Write( ',size:[%s,%s,%s]' % o.size );
-#do not need length prop.			Write( ',vertex:{ length:' + str(len(o.data.verts)) );
-			Write( ',vertex:{');
-			# format: x, y, z, normal.x, normal.y, normal.z, u, v
+			Write( '"center":[%s,%s,%s],' % (objectCenter[0],objectCenter[1],objectCenter[2]) );
+			Write( '"rotation":[%s,%s,%s],' % (o.RotX, o.RotY, o.RotZ) );
+			Write( '"size":[%s,%s,%s],' % o.size );
+
+			Write( '"drawType":%s,' % o.drawType ); # 1 - Bounding box, 2 - wire, 3 - Solid, 4- Shaded, 5 - Textured. 
+
+			Write( '"vertex":{');
 			for v in o.data.verts:
-				Write('%s:[%s,%s,%s,%s,%s,%s,%s,%s,0],' % (v.index, v.co[0],v.co[1],v.co[2],v.no[0],v.no[1],v.no[2],v.uvco[0],v.uvco[1]))
-			Write( '}' );
+				Write('"%s":[%s,%s,%s,%s,%s,%s,%s,%s,0],' % (v.index, v.co[0],v.co[1],v.co[2],v.no[0],v.no[1],v.no[2],v.uvco[0],v.uvco[1])) # index: x, y, z, normal.x, normal.y, normal.z, u, v
+			Write( '},' );
 
-#
-#			faceCount = 0
-#			for f in o.data.faces:
-#				if len(f.v) == 4:
-#					faceCount += 2
-#				else:
-#					faceCount += 1
-#			WriteLn( ',face:{ length:' + str(faceCount) );
-#
-
-			Write( ',face:[' );
+			Write( '"face":[' );
 			for f in o.data.faces:
-				faceData = '%s,%s,%s,%s,%s' % (f.no[0],f.no[1],f.no[2],f.materialIndex,f.smooth )
+				faceData = '%s,%s,%s,%s,%s' % (f.no[0], f.no[1], f.no[2], f.materialIndex, f.smooth ) # , f.image.name
 
 				if len(f.v) == 4:
-					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[1].index, f.v[2].index) + ',' + faceData +']' )
-					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[2].index, f.v[3].index) + ',' + faceData +']' )
+					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[1].index, f.v[2].index) + faceData +']' )
+					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[2].index, f.v[3].index) + faceData +']' )
 				else:
-					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[1].index, f.v[2].index) + ' ' + faceData +']' )
-			Write( ']' );
+					Write(',[%s,%s,%s,' % (f.v[0].index, f.v[1].index, f.v[2].index) + faceData +']' )
+			Write( '],' );
 
 
 			#material: http://www.blender.org/modules/documentation/237PythonDoc/Material.Material-class.html#specTransp
@@ -100,7 +94,7 @@ def exportToFile(filename):
 #			WriteLn('#materialCount')
 #			WriteLn( str(materialCount) )
 
-			Write(',material:[')
+			Write('"material":[')
 			for i in range(0,materialCount,1):
 				m = o.data.materials[i]
 
@@ -120,10 +114,10 @@ def exportToFile(filename):
 				
 				# miss .mode ?
 
-			Write( ']' );
+			Write( '],' );
 			Write('},');
 		
-	Write( '})' );
+	Write( '}' );
 
 	end = Blender.sys.time()
 	message = 'Successfully exported "%s" in %.4f seconds' % ( Blender.sys.basename(filename), end-start)
