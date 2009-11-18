c635b38c86c559ef44a4eb9c0cbed123e94b4595
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2009-11-18 14:49:04 +0000

    - testing:   "var f = new Function(\"var myVar = 123; function foo() { Print(EvalVarByName('myVar')) }; foo()\"); f()"

--- src/tools/jsTest.cpp
+++ src/tools/jsTest.cpp
@@ -1,43 +1,7 @@
 #define XP_WIN
-
 #include <jsapi.h>
+#include <jsdbgapi.h>
 #include <cstring>
-
-#include "../common/jlplatform.h"
-#include "../common/jlhelper.h"
-
-jl_malloc_t jl_malloc = malloc;
-jl_calloc_t jl_calloc = calloc;
-jl_memalign_t jl_memalign = memalign;
-jl_realloc_t jl_realloc = realloc;
-jl_msize_t jl_msize = msize;
-jl_free_t jl_free = free;
-
-
-
-int index = 1234;
-
-static JSBool MyConstructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
-	
-	return JS_TRUE;
-}
-
-JSBool MyProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
-
-	if ( *vp != JSVAL_VOID )
-		return JS_TRUE;
-
-//	JS_SetProperty(cx, obj, id, vp);
-	//JS_SetProperty(cx, obj, "myProperty", vp);
-
-	*vp = INT_TO_JSVAL(index);
-
-//	JS_DefineProperty(cx, obj, "myProperty", *vp, NULL, NULL, 0);
-
-
-	index++;
-	return JS_TRUE;
-}
 
 JSBool Print(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
 	
@@ -45,25 +9,28 @@ JSBool Print(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	return JS_TRUE;
 }
 
-JSBool JL_DefineClassProperties(JSContext *cx, JSObject *obj, JSPropertySpec *ps) {
+JSBool EvalVarByName(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+	
+	JS_ASSERT( JSVAL_IS_STRING(argv[0]) );
+	
+	const char *name = JS_GetStringBytes(JSVAL_TO_STRING(argv[0]));
+	JS_ASSERT( name );
 
-	JSBool ok;
-	for (ok = JS_TRUE; ps->name; ps++) {
+	JSStackFrame *fp = JS_GetScriptedCaller(cx, NULL);
+	JS_ASSERT( fp );
 
-		if ( ps->tinyid >= 0 ) {
-			ok = JS_DefineProperty(cx, obj, ps->name, JSVAL_VOID, ps->getter, ps->setter, ps->flags);
-			if (!ok)
-				break;
-		} else {
-			ok = JS_DefinePropertyWithTinyId(cx, obj, ps->name, ps->tinyid, JSVAL_VOID, ps->getter, ps->setter, ps->flags);
-			if (!ok)
-				break;
-		}
+	JSBool found;
+	JSObject *scope = JS_GetFrameScopeChain(cx, fp);
+	for ( ; scope; scope = JS_GetParent(cx, scope) ) {
+
+		JS_HasProperty(cx, scope, name, &found);
+		if ( found )
+			return JS_GetProperty(cx, scope, name, rval);
 	}
-	return ok;
-}
 
-bool _unsafeMode = false;
+	*rval = JSVAL_VOID;
+	return JS_TRUE;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -77,28 +44,28 @@ int main(int argc, char* argv[]) {
 	JS_InitStandardClasses(cx, globalObject);
 
 	JS_SetVersion(cx, (JSVersion)JSVERSION_LATEST);
-	JS_SetOptions(cx, JS_GetOptions(cx) | JSOPTION_VAROBJFIX);
+	//	JS_SetOptions(cx, JS_GetOptions(cx) | JSOPTION_VAROBJFIX | JSOPTION_ANONFUNFIX | JSOPTION_JIT); // same result with these options
 
 	JS_DefineFunction(cx, globalObject, "Print", Print, 0, 0);
+	JS_DefineFunction(cx, globalObject, "EvalVarByName", EvalVarByName, 0, 0);
 
-	JSClass myClass = { "MyClass", 0, JS_PropertyStub , JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub, JSCLASS_NO_OPTIONAL_MEMBERS };
-	JSPropertySpec ps[] = { {"myProperty", 0, JSPROP_SHARED, MyProperty, NULL}, {NULL} };
-
-	JS_InitClass(cx, globalObject, NULL, &myClass, MyConstructor, 0, ps, NULL, NULL, NULL);
-
-	JLLoadScript(cx, globalObject, "test.js", true, false);
-
-
-/*
 	char scriptSrc[] =
-"var oa = new MyClass; Print(oa.myProperty);   var ob = new MyClass; Print(oa.myProperty)"; // prints 11, expect 12
+	"var f = new Function(\"var myVar = 123; function foo() { Print(EvalVarByName('myVar')) }; foo()\"); f()";
+
+	// the following is working:
+	// "var f = new Function(\"var myVar = 123; function foo() { Print(myVar) }; foo()\"); f()";
+	//                                                                 ~~~~~
 
 	JSScript *script = JS_CompileScript(cx, globalObject, scriptSrc, strlen(scriptSrc), "mytest", 1);
+	JS_ASSERT( script );
+
 	jsval rval;
 	JSBool st = JS_ExecuteScript(cx, globalObject, script, &rval);
-*/
+	JS_ASSERT( st );
+
 	JS_DestroyContext(cx);
 	JS_DestroyRuntime(rt);
 	JS_ShutDown();
+
 	return EXIT_SUCCESS;
 }
