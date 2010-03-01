896711b354366fa7b0f77e2e8a5e409c8c80c367
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2010-03-01 19:09:54 +0000

    - update the demo.

--- tests/arabesques.js
+++ tests/arabesques.js
@@ -1,104 +1,92 @@
 try { 
 
-LoadModule('jsdebug');
 LoadModule('jsstd');
 LoadModule('jssdl');
 LoadModule('jsgraphics');
-LoadModule('jsfont');
-LoadModule('jsoglft');
+LoadModule('jsprotex');
 
 GlSetAttribute( GL_DOUBLEBUFFER, 1 );
 GlSetAttribute( GL_SWAP_CONTROL, 1 ); // vsync
-GlSetAttribute( GL_DEPTH_SIZE, 16 );
 GlSetAttribute( GL_ACCELERATED_VISUAL, 1 );
+SetVideoMode(640,480, undefined, OPENGL);
 
-unicodeKeyboardTranslation = true;
-keyRepeatDelay = 300;
-keyRepeatInterval = 50;
-SetVideoMode(1280, 1024, 32, HWACCEL | OPENGL | RESIZABLE | FULLSCREEN, false);
 maxFPS = 60;
 
-Ogl.MatrixMode(Ogl.PROJECTION);
-//Ogl.Ortho(-1, 1, -1, 1, 0.01, 100);
-Ogl.Perspective(60, 0.01, 1000);
-Ogl.MatrixMode(Ogl.MODELVIEW);
 
-Ogl.Enable(Ogl.DEPTH_TEST);
-Ogl.DepthFunc(Ogl.LEQUAL);
+const curveGaussian = function(c) { return function(x) { return Math.exp( -(x*x)/(2*c*c) ) } }
+var tex = new Texture(16,16, 1).Set(0).AddGradiantRadial( curveGaussian( 0.5 ) );
 
-//Ogl.Enable(Ogl.LIGHTING);
-Ogl.Enable(Ogl.LIGHT0);
+with (Ogl) {
 
-Ogl.ShadeModel(Ogl.SMOOTH);
-Ogl.Material(Ogl.FRONT, Ogl.SPECULAR, [1.0, 1.0, 1.0, 1.0]);
-Ogl.Material(Ogl.FRONT, Ogl.SHININESS, 50);
+	Enable( TEXTURE_2D );
+	var textureId = GenTexture();
+	BindTexture(TEXTURE_2D, textureId);
+	DefineTextureImage(TEXTURE_2D, ALPHA, tex);
+	TexParameter(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR);
+	TexParameter(TEXTURE_2D, TEXTURE_MAG_FILTER, LINEAR);
+	Enable(POINT_SPRITE);
+	TexEnv(POINT_SPRITE, COORD_REPLACE, TRUE);
+	Enable(BLEND);
+	BlendFunc(SRC_ALPHA, ONE);
+	PointSize(6);
+	ClearDepth(0);
+	ClearColor(0,0,0,0);
+	MatrixMode(PROJECTION);
+	Ortho(-10, 10, -10, 10, 0, 1);
+	MatrixMode(MODELVIEW);
+}
 
-Ogl.ColorMaterial ( Ogl.FRONT_AND_BACK, Ogl.AMBIENT_AND_DIFFUSE ) ;
-Ogl.Enable ( Ogl.COLOR_MATERIAL ) ;
-Ogl.Color(1,1,1);
-
-Ogl.Enable(Ogl.CULL_FACE);
-Ogl.CullFace(Ogl.FRONT);
-
-//Ogl.Enable(Ogl.BLEND);
-//Ogl.BlendFunc(Ogl.SRC_ALPHA, Ogl.ONE_MINUS_SRC_ALPHA);
-//Ogl.Enable(Ogl.TEXTURE_2D);
-
-var f = new Font('c:\\windows\\fonts\\arial.ttf');
-var f3d = new Font3D(f, Font3D.FILLED, 12);
-//f3d.tessellationSteps = 10;
-Ogl.PixelStore( Ogl.UNPACK_ALIGNMENT, 1 );
-
-Ogl.LineWidth(1);
 
 var frame = 0;
-
-var listenerList = [];
-
-
 var sparkList = [];
 
+function Dist(x0,y0, x1,y1) Math.sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0));
+var Int = Math.floor;
 function Rnd(min, max) Math.random() * (max-min) + min;
 function Rndi(min, max) Math.floor(Math.random() * (max-min) + min);
-
 function Rndv(val, range) val + (Math.random() * range*2) - range;
 
 
-var total = 0;
 
 function Spark(x,y,angle,dist) {
 	
 	var lines = [x,y];
 	var count = 1;
 	
-	var delta = 0;
+	var splitAt = Int(Rndv(10, 9));
+	var dieAt = Int(Rndv(50, 10));
 	
-	var splitAt = Rndv(5, 1);
-	var dieAt = Math.floor(Rndv(200, 5));
+	var dustAt = 200;
 	
-	var len = Rndv(0.2, 0.07);
-	var deltaVar = Rndv(0.04, 0.04);
-	var way = Math.random() > 0.5 ? 1 : -1;
+	var len = Rnd(0.05, 0.2);
+	var deltaVar = Rnd(0, 0.01);
 	
-	listenerList.push( function() sparkList.push( new Spark(x,y,angle, dist + count) ) );
+	var m = Ogl.UnProject(mouseX, mouseY);
+	var d1 = Dist( m[0], m[1], x + Math.cos(angle+deltaVar)*len, y + Math.sin(angle+deltaVar)*len );
+	var d2 = Dist( m[0], m[1], x + Math.cos(angle-deltaVar)*len, y + Math.sin(angle-deltaVar)*len );
+	var way = d1 > d2 ? -1 : 1;
+	
+//	var way = Math.random() > 0.5 ? 1 : -1;
+	
+
+	var r = Rnd(0,1);
+	var g = Rnd(0,1);
+	var b = Rnd(0,1);
 	
 	var callList;
 	for (;;) {
-/*		
-		for ( var i = 1; i < count; i++ ) {
-			
-//			Ogl.Color(10 - dist/100);
-			Ogl.LineWidth(5 - (dist+i)/5);
-			Ogl.Begin( Ogl.LINES );
-			Ogl.Vertex(lines[i*2-2],lines[i*2-1]);
-			Ogl.Vertex(lines[i*2],lines[i*2+1]);
-			Ogl.End();
+
+//		Ogl.LineWidth(1+count/dustAt);
+
+		var li = 1-count/dustAt;
+		Ogl.Color( li* (deltaVar*10), li* (deltaVar*10), li > 0.5 ? li+1 : li*2 );
+
+		if ( count == dustAt ) {
+		
+			if ( callList )
+				Ogl.DeleteList(callList);
+			return;
 		}
-*/
-
-//		Ogl.LineWidth(total10-dist/100);
-
-//		Ogl.Color(dist/100);
 
 		if ( callList ) {
 			
@@ -110,9 +98,9 @@ function Spark(x,y,angle,dist) {
 
 			if ( count <= dieAt ) {
 
-				Ogl.Begin( Ogl.LINE_STRIP );
+				Ogl.Begin( Ogl.POINTS ); // LINE_STRIP
 				for ( var i = 0; i < count; i++ )
-					Ogl.Vertex(lines[i*2],lines[i*2+1], (dist+i)/100);
+					Ogl.Vertex(lines[i*2],lines[i*2+1], 0);
 				Ogl.End();
 			}
 
@@ -123,64 +111,62 @@ function Spark(x,y,angle,dist) {
 
 		if ( count < dieAt ) {
 
-			delta += deltaVar;
-
-			angle += delta * way;
-
 			x += Math.cos(angle)*len;
 			y += Math.sin(angle)*len;
 
-			if ( count > splitAt ) {
-
-//				sparkList.push( new Spark(x,y,angle, dist + count) );
-				
-//				splitAt = count + Rndv(40, 5);
-//				splitAt = 999;
-			}
-
-
+			angle += deltaVar * count * way;
 
 			lines.push(x,y);
-			count++;
-			total++;
 		}
-		
-		yield;
-	
-	}
 
+		if ( count == splitAt ) {
+
+			sparkList.push( new Spark(x,y,angle, dist + count) );
+		}
+	
+		count++;
+		yield;
+	}
 }
 
 
 sparkList.push(new Spark(0,0,Math.PI/2, 0) );
 
 
-
 function SurfaceReady() {
-
+	
 	frame++;
 
-	Ogl.Clear(Ogl.COLOR_BUFFER_BIT | Ogl.DEPTH_BUFFER_BIT);
-	Ogl.LoadIdentity();
-	
-//	Ogl.Translate(0,-0.5, -frame / 50 -10 );
-	
-	Ogl.LookAt(Math.cos(frame/100)*10, Math.sin(frame/100)*10, frame/100 + 5, 0,0,5, 0,0,1);
-	
+	Ogl.Clear(Ogl.COLOR_BUFFER_BIT|Ogl.DEPTH_BUFFER_BIT);
+
 	for each ( spark in sparkList )
-		spark.next();
-	
+		try {
+			spark.next();
+		} catch( ex if ex instanceof StopIteration) {}
+
+
+	Ogl.Flush();
+	Ogl.Finish();
 
 	GlSwapBuffers(true);
 }
 
 
 var listeners = {
-	onKeyDown:function(sym, mod, char) {
+	onMouseButtonDown:function(b, x, y) {
 		
-		for each ( listener in listenerList )
-			listener(sym, mod, char);
-
+		var up = Ogl.UnProject(x,y);
+		sparkList.push(new Spark(up[0], up[1], Math.PI/2, 0) );
+	},
+	onKeyDown:function(sym, mod, chr) {
+		
+		if ( sym == K_SPACE ) {
+			
+			ProcessEvents( SurfaceReadyEvents() );
+			SetVideoMode(desktopWidth, desktopHeight, undefined, OPENGL | RESIZABLE | (videoFlags & FULLSCREEN ? 0 : FULLSCREEN));
+			Ogl.Viewport(0, 0, videoWidth, videoHeight);
+		}
+		
 		if ( sym == K_ESCAPE )
 			endSignal = true;
 	},
@@ -190,13 +176,13 @@ var listeners = {
 	},
 	onVideoResize: function(w, h) {
 
-		ProcessEvents( SurfaceReadyEvents() );
 		Ogl.Viewport(0, 0, w, h);
 	},
 };
 
+
 while ( !endSignal )
-	var e = ProcessEvents( SDLEvents(listeners), EndSignalEvents(), SurfaceReadyEvents(SurfaceReady) );
+	ProcessEvents( SDLEvents(listeners), EndSignalEvents(), SurfaceReadyEvents(SurfaceReady) );
 
 } catch(ex) {
 
