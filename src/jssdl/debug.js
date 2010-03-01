0a3500149bd99dc11a22e200d6f8185f0fbf7d9c
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2010-03-01 23:14:01 +0000

    - movable shapes.

--- src/jssdl/debug.js
+++ src/jssdl/debug.js
@@ -7,175 +7,158 @@ LoadModule('jssdl');
 LoadModule('jsgraphics');
 LoadModule('jsprotex');
 
-GlSetAttribute( GL_DOUBLEBUFFER, 1 );
-GlSetAttribute( GL_SWAP_CONTROL, 1 ); // vsync
-GlSetAttribute( GL_DEPTH_SIZE, 16 );
-GlSetAttribute( GL_ACCELERATED_VISUAL, 1 );
-
 unicodeKeyboardTranslation = true;
 keyRepeatDelay = 300;
 keyRepeatInterval = 50;
 
-SetVideoMode(320, 200, undefined, HWACCEL | OPENGL | RESIZABLE);
+GlSetAttribute( GL_DOUBLEBUFFER, 1 );
+GlSetAttribute( GL_SWAP_CONTROL, 1 ); // vsync
+GlSetAttribute( GL_DEPTH_SIZE, 16 );
+GlSetAttribute( GL_ACCELERATED_VISUAL, 1 );
+SetVideoMode(800, 600, 0, OPENGL | RESIZABLE);
 
-maxFPS = 60;
+//maxFPS = 60;
 
+//Ogl.Viewport(0,0,videoWidth, videoHeight);
 Ogl.MatrixMode(Ogl.PROJECTION);
-Ogl.Ortho(-10, 10, -10, 10, 0.01, 1000);
-//Ogl.Perspective(60, 0.01, 1000);
+Ogl.Ortho(0, videoWidth, videoHeight, 0, 0, 1000);
 Ogl.MatrixMode(Ogl.MODELVIEW);
 
-
-const curveGaussian = function(c) { return function(x) { return Math.exp( -(x*x)/(2*c*c) ) } }
-var tex = new Texture(16,16, 1).Set(0).AddGradiantRadial( curveGaussian( 0.5 ) );
+var frame = 0;
 
 
-with (Ogl) {
-	Enable( TEXTURE_2D );
-	var textureId = GenTexture();
-	BindTexture(TEXTURE_2D, textureId);
-	DefineTextureImage(TEXTURE_2D, ALPHA, tex);
-	TexParameter(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR);
-	TexParameter(TEXTURE_2D, TEXTURE_MAG_FILTER, LINEAR);
-	//		PointParameter(POINT_DISTANCE_ATTENUATION, [0, 0, 0.01]); // 1/(a + b*d + c *d^2)
-	Enable(POINT_SPRITE);
-	TexEnv(POINT_SPRITE, COORD_REPLACE, TRUE);
-	//		TexEnv(TEXTURE_ENV, TEXTURE_ENV_MODE, MODULATE);
-			Enable(BLEND);
-	//		BlendFunc(SRC_ALPHA, ONE_MINUS_SRC_ALPHA);
+var objects = [];
 
-// with LUMINANCE only		
-	//BlendFunc(SRC_ALPHA, ONE); // radioactive cloud
-	BlendFunc(SRC_ALPHA, ONE); // less radioactive cloud
-	//BlendFunc(ONE_MINUS_DST_COLOR, ONE); // normal cloud
-	//BlendFunc(ONE_MINUS_SRC_COLOR, ONE_MINUS_SRC_COLOR); // normal strange 1
-	//BlendFunc(ONE_MINUS_DST_COLOR, ONE_MINUS_SRC_COLOR); // normal strange 2
-	//BlendFunc(ZERO, ONE_MINUS_SRC_COLOR); // dark cloud
-	//BlendFunc(SRC_COLOR, ONE_MINUS_SRC_COLOR); // less dark cloud
+var evListeners = {};
 
-	PointSize(6);
+function AddEventListener(type, fct) {
+
+	var listeners = evListeners[type] || (evListeners[type] = []);
+	listeners.push(fct);
 }
 
+function RemoveEventListener(type, fct) {
 
-var frame = 0;
-var listenerList = [];
-var sparkList = [];
+	evListeners[type].splice(evListeners[type].indexOf(fct), 1);
+}
 
-function Dist(x0,y0, x1,y1) Math.sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0));
-var Int = Math.floor;
-function Rnd(min, max) Math.random() * (max-min) + min;
-function Rndi(min, max) Math.floor(Math.random() * (max-min) + min);
+function FireListeners(evName /*, ...*/) {
 
-function Rndv(val, range) val + (Math.random() * range*2) - range;
-
-
-function Spark(x,y,angle,dist) {
+	var listeners = evListeners[evName];
+	if ( listeners ) {
 	
-	var lines = [x,y];
-	var count = 1;
-	
-	var splitAt = Int(Rndv(10, 9));
-	var dieAt = Int(Rndv(50, 10));
-	
-	var dustAt = 200;
-	
-	var len = Rnd(0.05, 0.2);
-	var deltaVar = Rnd(0, 0.01);
-	
-	var m = Ogl.UnProject(mouseX, mouseY);
-	var d1 = Dist( m[0], m[1], x + Math.cos(angle+deltaVar)*len, y + Math.sin(angle+deltaVar)*len );
-	var d2 = Dist( m[0], m[1], x + Math.cos(angle-deltaVar)*len, y + Math.sin(angle-deltaVar)*len );
-	var way = d1 > d2 ? -1 : 1;
-	
-//	var way = Math.random() > 0.5 ? 1 : -1;
-	
-//	listenerList.push( function() sparkList.push( new Spark(x,y,angle, dist + count) ) );
-	
-	var r = Rnd(0,1);
-	var g = Rnd(0,1);
-	var b = Rnd(0,1);
-	
-	var callList;
-	for (;;) {
-
-//		Ogl.LineWidth(1+count/dustAt);
-
-		var li = 1-count/dustAt;
-		Ogl.Color( li* (deltaVar*10), li* (deltaVar*10), li > 0.5 ? li+1 : li*2 );
-
-		if ( count == dustAt ) {
-		
-			if ( callList )
-				Ogl.DeleteList(callList);
-			return;
-		}
-
-		if ( callList ) {
-			
-			Ogl.CallList(callList);
-		} else {
-
-			if ( count == dieAt )
-				callList = Ogl.NewList();
-
-			if ( count <= dieAt ) {
-
-				Ogl.Begin( Ogl.POINTS ); // LINE_STRIP
-				for ( var i = 0; i < count; i++ )
-					Ogl.Vertex(lines[i*2],lines[i*2+1], (dist+i)/100);
-				Ogl.End();
-			}
-
-			if ( count == dieAt )
-				Ogl.EndList();
-		}
-
-
-		if ( count < dieAt ) {
-
-			x += Math.cos(angle)*len;
-			y += Math.sin(angle)*len;
-
-			angle += deltaVar * count * way;
-
-			lines.push(x,y);
-		}
-
-		if ( count == splitAt ) {
-
-			sparkList.push( new Spark(x,y,angle, dist + count) );
-		}
-	
-		count++;
-		yield;
+		for each ( var fct in listeners )
+			if ( fct.apply(this, arguments) === false )
+				return;
 	}
 }
 
-sparkList.push(new Spark(0,0,Math.PI/2, 0) );
+function SetForeground(obj) {
+	
+	var pos = objects.indexOf(obj);
+	if ( pos == 0 )
+		return;
+	objects.splice(pos, 1);
+	objects.unshift(obj);
+}
+
+////////////////////////
+
+function Quad(color) {
+
+	var _this = this;
+	
+	var px = 100;
+	var py = 100;
+	var w = 100;
+	var h = 50;
+	var border = 2;
+	
+	var clist;
+
+	this.Draw = function() {
+
+		Ogl.PushMatrix();
+		Ogl.Translate(px, py);
+		if ( clist ) {
+			
+			Ogl.CallList(clist);
+		} else {
+
+			clist = Ogl.NewList(Ogl.COMPILE);
+			Ogl.Color(color);
+			Ogl.Begin(Ogl.QUADS);
+			Ogl.Vertex(0,0);  Ogl.Vertex(w,0);  Ogl.Vertex(w,h);  Ogl.Vertex(0,h);
+			Ogl.End();
+
+			Ogl.Color(1);
+			Ogl.LineWidth(border);
+			Ogl.Begin(Ogl.LINE_LOOP);
+			Ogl.Vertex(0,0);  Ogl.Vertex(w,0);  Ogl.Vertex(w,h);  Ogl.Vertex(0,h);
+			Ogl.End();
+			Ogl.EndList();
+		}
+		Ogl.PopMatrix();
+	}
+	
+	this.HasPoint = function(x,y) {
+		
+		return x >= px && y >= py && x <= px+w && y <= py+h;
+	}
+	
+	function onmousedown(ev, b, x,y) {
+
+		if ( !_this.HasPoint(x,y) )
+			return true;
+		
+		SetForeground(_this);
+		
+		var dx = x-px;
+		var dy = y-py;
+		
+		function onmousemove(ev, x1,y1) {
+		
+			px = x1-dx;
+			py = y1-dy;
+		}
+		
+		function onmouseup(ev, b, x,y) {
+			
+			RemoveEventListener('mousemove', onmousemove);
+			RemoveEventListener('mouseup', onmouseup);
+		}
+		
+		AddEventListener('mousemove', onmousemove);
+		AddEventListener('mouseup', onmouseup);
+		return false;
+	}
+	
+//	AddEventListener('mousedown', onmousedown);
+	this.onmousedown = onmousedown;
+}
+
+
+for ( var i = 0; i < 100; i++ ) {
+
+	objects.push(new Quad([1,0,0]));
+	objects.push(new Quad([0,1,0]));
+	objects.push(new Quad([0,0,1]));
+}
+
+////////////////////////
 
 function SurfaceReady() {
 	
 	frame++;
-
-	Ogl.Clear(Ogl.COLOR_BUFFER_BIT);
+	
+	Ogl.Clear(Ogl.COLOR_BUFFER_BIT|Ogl.DEPTH_BUFFER_BIT);
 	Ogl.LoadIdentity();
-	
-	Ogl.Translate(0,-0.5, -frame / 50 -10 );
-//	Ogl.Translate(0,0,Rnd(-0.1,0.1) );
-//	Ogl.LookAt(Math.cos(frame/100)*10, Math.sin(frame/100)*10, frame/100 + 5, 0,0,5, 0,0,1);
-	
-	for each ( spark in sparkList )
-		try {
-			spark.next();
-		} catch( ex if ex instanceof StopIteration) {}
 
+	for ( var i = objects.length-1; i >= 0; --i )
+		objects[i].Draw();
 
 	Ogl.Flush();
 	Ogl.Finish();
-
-	// Ogl.ReadBuffer( Ogl.BACK );
-	//	new File('movie'+frame+'.png').content = EncodePngImage(Ogl.RenderToImage());
-	// Create a movie file from single image files (png, jpegs) - http://www.miscdebris.net/blog/2008/04/28/create-a-movie-file-from-single-image-files-png-jpegs/
 
 	GlSwapBuffers(true);
 }
@@ -184,22 +167,40 @@ function SurfaceReady() {
 var listeners = {
 	onMouseButtonDown:function(b, x, y) {
 		
-		var up = Ogl.UnProject(x,y);
-		sparkList.push(new Spark(up[0], up[1], 0, 0) );
-//		Print(up,'\n');
+		var len = objects.length;
+		for ( var i = 0; i < len; ++i ) {
+			
+			var o = objects[i];
+			if ( o.onmousedown && o.onmousedown('onmousedown', b, x, y) === false )
+				break;
+		}
+		
+		FireListeners('mousedown', b, x, y);
 	},
-	onKeyDown:function(sym, mod, char) {
+	onMouseButtonUp:function(b, x, y) {
+		
+		FireListeners('mouseup', b, x, y);
+	},
+	onMouseMotion:function(x, y, relx, rely, state, mod) {
+	
+//		[x,y] = Ogl.UnProject(x,y);
+		FireListeners('mousemove', x, y);
+	},
+	onKeyDown:function(sym, mod, chr) {
+
+		FireListeners('keydown',sym, mod, chr);
 		
 		if ( sym == K_SPACE ) {
 			
 			ProcessEvents( SurfaceReadyEvents() );
-			SetVideoMode(desktopWidth, desktopHeight, 3, HWACCEL | OPENGL | RESIZABLE | (videoFlags & FULLSCREEN ? 0 : FULLSCREEN));
+			SetVideoMode(desktopWidth, desktopHeight, undefined, OPENGL | RESIZABLE | (videoFlags & FULLSCREEN ? 0 : FULLSCREEN));
 			Ogl.Viewport(0, 0, videoWidth, videoHeight);
+			Ogl.MatrixMode(Ogl.PROJECTION);
+			Ogl.LoadIdentity();
+			Ogl.Ortho(0, videoWidth, videoHeight, 0, 0, 1000);
+			Ogl.MatrixMode(Ogl.MODELVIEW);
 		}
 		
-		for each ( listener in listenerList )
-			listener(sym, mod, char);
-
 		if ( sym == K_ESCAPE )
 			endSignal = true;
 	},
@@ -208,8 +209,12 @@ var listeners = {
 		endSignal = true;
 	},
 	onVideoResize: function(w, h) {
-
+	
 		Ogl.Viewport(0, 0, w, h);
+		Ogl.MatrixMode(Ogl.PROJECTION);
+		Ogl.LoadIdentity();
+		Ogl.Ortho(0, w, h, 0, 0, 1000);
+		Ogl.MatrixMode(Ogl.MODELVIEW);
 	},
 };
 
