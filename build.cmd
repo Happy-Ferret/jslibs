72a5cafbe201f53f3d012ad3090e7dcea64204b8
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2010-05-17 21:26:21 +0000

    - switch from Visual Studio Express 8 (2005) to Visual Studio Express 9 (2008).

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
 
-set MOZ_BUILD_SCRIPT=C:\TOOLS\mozilla-build\start-msvc8.bat
+set MOZ_BUILD_SCRIPT=C:\TOOLS\mozilla-build\start-msvc9.bat
 
 rem ==========================================================================
 for %%A in ("%CD%") do set _CD_SHORT=%%~sfA
 echo cd "%_CD_SHORT%";sh build.sh "%1" | call %MOZ_BUILD_SCRIPT%
-pause
