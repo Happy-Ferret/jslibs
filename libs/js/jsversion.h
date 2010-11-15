c6fda2662428214533710203881959f9bdfc5959
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2010-11-15 21:48:17 +0000

    - use JaegerMonkey engine.

--- libs/js/jsversion.h
+++ libs/js/jsversion.h
@@ -4,20 +4,15 @@
 #define JS_HAS_PERL_SUBSTR      1       /* has str.substr */
 #define JS_HAS_OBJ_PROTO_PROP   1       /* has o.__proto__ etc. */
 #define JS_HAS_OBJ_WATCHPOINT   1       /* has o.watch and o.unwatch */
-#define JS_HAS_EVAL_THIS_SCOPE  1       /* Math.eval is same as with (Math) */
 #define JS_HAS_SHARP_VARS       1       /* has #n=, #n# for object literals */
-#define JS_HAS_SCRIPT_OBJECT    1       /* has (new Script("x++")).exec() */
 #define JS_HAS_XDR              1       /* has XDR API and internal support */
-#define JS_HAS_XDR_FREEZE_THAW  1       /* has XDR freeze/thaw script methods */
 #define JS_HAS_TOSOURCE         1       /* has Object/Array toSource method */
 #define JS_HAS_DEBUGGER_KEYWORD 1       /* has hook for debugger keyword */
 #define JS_HAS_CATCH_GUARD      1       /* has exception handling catch guard */
 #define JS_HAS_SPARSE_ARRAYS    0       /* array methods preserve empty elems */
-#define JS_HAS_GETTER_SETTER    1       /* has JS2 getter/setter functions */
 #define JS_HAS_UNEVAL           1       /* has uneval() top-level function */
 #define JS_HAS_CONST            1       /* has JS2 const as alternative var */
 #define JS_HAS_FUN_EXPR_STMT    1       /* has function expression statement */
-#define JS_HAS_LVALUE_RETURN    1       /* has o.item(i) = j; for native item */
 #define JS_HAS_NO_SUCH_METHOD   1       /* has o.__noSuchMethod__ handler */
 #define JS_HAS_XML_SUPPORT      1       /* has ECMAScript for XML support */
 #define JS_HAS_ARRAY_EXTRAS     1       /* has indexOf and Lispy extras */
@@ -27,10 +22,27 @@
 #define JS_HAS_GENERATOR_EXPRS  1       /* has (expr for (lhs in iterable)) */
 #define JS_HAS_EXPR_CLOSURES    1       /* has function (formals) listexpr */
 
-
 /* Features that are present in all versions. */
 #define JS_HAS_RESERVED_JAVA_KEYWORDS   1
 #define JS_HAS_RESERVED_ECMA_KEYWORDS   1
 
+/* Support for JS_NewGlobalObject. */
+#define JS_HAS_NEW_GLOBAL_OBJECT        1
+
+/* Support for JS_MakeSystemObject. */
+#define JS_HAS_MAKE_SYSTEM_OBJECT       1
+
 /* Feature-test macro for evolving destructuring support. */
 #define JS_HAS_DESTRUCTURING_SHORTHAND  (JS_HAS_DESTRUCTURING == 2)
+/*
+ * Feature for Object.prototype.__{define,lookup}{G,S}etter__ legacy support;
+ * support likely to be made opt-in at some future time.
+ */
+#define OLD_GETTER_SETTER_METHODS       1
+
+// removed:
+// #define JS_HAS_EVAL_THIS_SCOPE  1       // Math.eval is same as with (Math)
+// #define JS_HAS_SCRIPT_OBJECT    1       // has (new Script("x++")).exec()
+// #define JS_HAS_XDR_FREEZE_THAW  1       // has XDR freeze/thaw script methods
+// #define JS_HAS_GETTER_SETTER    1       // has JS2 getter/setter functions
+// #define JS_HAS_LVALUE_RETURN    1       // has o.item(i) = j; for native item    // see. https://bugzilla.mozilla.org/show_bug.cgi?id=509098
