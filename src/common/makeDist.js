37f91c18c40bbd04ce8b9c04f992fd5b9f5e61bd
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2012-04-11 18:12:01 +0000

    - fix camel case.

--- src/common/makeDist.js
+++ src/common/makeDist.js
@@ -1,42 +1,47 @@
 LoadModule('jsstd');
 LoadModule('jsio');
 
+var svnDir = 'C:/Program Files/TortoiseSVN/bin/';
 
+function zip(dir, destFilename) {
 
-function Zip(dir, destFilename) {
-
-	Print( 'zip '+dir+' to '+destFilename, '\n' );
+	print( 'zip '+dir+' to '+destFilename, '\n' );
 	// eXclude Recursively .svn directories
 	// eXclude .tpl files
 	var p = new Process('7z', ['a', '-tzip', '-mx9', '-xr!*.tpl', '-xr!*.svn', destFilename, dir]);
-	for ( let data; data = p.stdout.Read(); )
-		Print(data);
-	for ( let data; data = p.stderr.Read(); )
-		Print(data);
+	for ( let data; data = p.stdout.read(); )
+		print(data);
+	for ( let data; data = p.stderr.read(); )
+		print(data);
 }
 
-function GetLatestRemoteSVNRevision() {
+function getLatestRemoteSVNRevision() {
 	
-	Print( 'Get latest remote SVN revision', '\n' );
-	var p = new Process('svn', ['--xml', 'info', '-r', 'HEAD']);
+	print( 'Get latest remote SVN revision', '\n' );
+	var p = new Process(svnDir+'svn', ['--xml', 'info', '-r', 'HEAD']);
 	var svnInfo = '';
-	for ( let data; data = p.stdout.Read(); )
+	for ( let data; data = p.stdout.read(); )
 		svnInfo += data;
 	svnInfo = new XML(svnInfo.replace(/<\?.*?\?>/, ''));
 	return svnInfo.entry.@revision;
 }
 
-function GetSVNWorkingCopyVersion() {
+function getSVNWorkingCopyVersion() {
 	
-	var p = new Process('svnversion');
-	var info = /(?:(\d+):)?(\d+)([MSP]*)/(p.stdout.Read());
-	return { mixed:Number(info[1]), wc:Number(info[2]), modified:info.indexOf('M')>0, switched:info.indexOf('S')>0, partial:info.indexOf('P')>0 };
+	var p = new Process(svnDir+'svnversion');
+	var info = /(?:(\d+):)?(\d+)([MSP]*)/(p.stdout.read());
+	return {
+		mixed: Number(info[1]),
+		wc: Number(info[2]),
+		modified: info.indexOf('M') > 0,
+		switched: info.indexOf('S') > 0,
+		partial: info.indexOf('P') > 0
+	};
 }
 
+function indentText(text, indent) [ indent+line for each (line in text.split('\n')) ].join('\n');
 
-function IndentText(text, indent) [ indent+line for each (line in text.split('\n')) ].join('\n');
-
-function GetLatestChanges() {
+function getLatestChanges() {
 
 	var host = 'jslibs.googlecode.com';
 	var page = '/svn/wiki/ReleaseNotes.wiki';
@@ -44,22 +49,22 @@ function GetLatestChanges() {
 	var response = '';
 	var soc = new Socket();
 	soc.nonblocking = true;
-	soc.Connect(host, 80);
+	soc.connect(host, 80);
 	soc.writable = function(s) {
 
 		delete soc.writable;
-		s.Write( 'GET '+page+' HTTP/1.0\r\nHost: '+host+'\r\nnUser-Agent: jslibs\r\nAccept: */*\r\nAccept-Charset: ISO-8859-1\r\nConnection: close\r\n\r\n' );
+		s.write( 'GET '+page+' HTTP/1.0\r\nHost: '+host+'\r\nnUser-Agent: jslibs\r\nAccept: */*\r\nAccept-Charset: ISO-8859-1\r\nConnection: close\r\n\r\n' );
 	}
 	soc.readable = function(s) {
 
-		var res = s.Read();
+		var res = s.read();
 		if ( res )
 			response += res;
 		else
 			delete soc.readable;
 	}
-	while( Poll([soc], 500) );
-	soc.Close();
+	while( poll([soc], 500) );
+	soc.close();
 	
 	var res = /== jslibs (\w+) ([0-9.?]+) (r[0-9?]+)? .*?==\n((?:.|\n)*?)\n==/img(response);
 	if ( res == null )
@@ -67,19 +72,18 @@ function GetLatestChanges() {
 	return res.slice(1);		
 }
 
+function getClassRevision(moduleName, className) {
 
-function GetClassRevision(moduleName, className) {
-
-  var proc = new Process('jshost', [ '-u', '-i', 'LoadModule("'+moduleName+'"); _configuration.stdout('+className+'._revision)']);
-  return Number(proc.stdout.Read());
+  var proc = new Process('jshost', [ '-u', '-i', 'var loadModule = host.loadModule; loadModule("'+moduleName+'"); host.stdout('+className+'._revision)']);
+  return Number(proc.stdout.read());
 }
 
 var copiedFilesDest = [];
 
-function Copy(fromFilename, toFilename) {
+function copy(fromFilename, toFilename) {
 	
-	Print( 'Copying '+fromFilename+' to '+toFilename+'\n' );
-	var fromFile = new File(fromFilename).Open(File.RDONLY);
+	print( 'Copying '+fromFilename+' to '+toFilename+'\n' );
+	var fromFile = new File(fromFilename).open(File.RDONLY);
 	var toFile = new File(toFilename);
 	
 	if ( toFile.exist && toFile.info.type == File.FILE_DIRECTORY ) {
@@ -90,22 +94,22 @@ function Copy(fromFilename, toFilename) {
 		else
 			toFile = new File(toFilename+'/'+fromFilename.substr(pos+1));
 	}
-	toFile.Open(File.WRONLY | File.CREATE_FILE | File.TRUNCATE);
-	for ( var buf; buf = fromFile.Read(65536); )
-		toFile.Write(buf);
-	toFile.Close();
+	toFile.open(File.WRONLY | File.CREATE_FILE | File.TRUNCATE);
+	for ( var buf; buf = fromFile.read(65536); )
+		toFile.write(buf);
+	toFile.close();
 	copiedFilesDest.push(toFile.name);
-	fromFile.Close();
+	fromFile.close();
 }
 
 
-function RecursiveDir(path, callback) {
+function recursiveDir(path, callback) {
 	
 	(function(path) {
 
 		var dir = new Directory(path);
-		dir.Open();
-		for ( var entry; ( entry = dir.Read(Directory.SKIP_BOTH) ); ) {
+		dir.open();
+		for ( var entry; ( entry = dir.read(Directory.SKIP_BOTH) ); ) {
 
 			var file = new File(dir.name+'/'+entry);
 			switch ( file.info.type ) {
@@ -117,7 +121,7 @@ function RecursiveDir(path, callback) {
 					break;
 			}
 		}
-		dir.Close();
+		dir.close();
 	})(path);
 }
 
@@ -133,29 +137,29 @@ switch ( arguments[1] ) {
 	case '--devsnapshot':
 		type = 'DEVSNAPSHOT';
 		jslibsVersion = '';
-		var info = GetSVNWorkingCopyVersion();
+		var info = getSVNWorkingCopyVersion();
 		jslibsRevision = info.wc;
 		changes = 'http://code.google.com/p/jslibs/source/list';
 		break;
 		
 	case '--version':
-		var [type, jslibsVersion, jslibsRevision, changes] = GetLatestChanges();
+		var [type, jslibsVersion, jslibsRevision, changes] = getLatestChanges();
 		changes = changes.replace(/<pre>|<\/pre>/g, ''); // cleanup
 		changes = changes.replace(/\n/g, '\r\n');
 		if ( jslibsRevision[0] == 'r' )
 			jslibsRevision = jslibsRevision.substr(1);
-		//jslibsRevision = 'r'+GetLatestRemoteSVNRevision();
+		//jslibsRevision = 'r'+getLatestRemoteSVNRevision();
 		break;
 	default:
-		Print( 'choose a type\n' );
+		print( 'choose a type\n' );
 		throw 0;
 }
 
 
 
-var readme = Expand(new File('./dist/readme.txt.tpl').content, function(id) {
+var readme = expand(new File('./dist/readme.txt.tpl').content, function(id) {
 	
-	switch(id) {
+	switch ( id ) {
 		case 'type':
 			return type;
 		case 'version':
@@ -163,56 +167,56 @@ var readme = Expand(new File('./dist/readme.txt.tpl').content, function(id) {
 		case 'revision':
 			return jslibsRevision;
 		case 'changes':
-			return IndentText(changes, '  ');
+			return indentText(changes, '  ');
 	}
 });
 
 
 
-Copy('qa.js', './dist/tests');
+copy('qa.js', './dist/tests');
 
-RecursiveDir('./src', function(file) /_qa\.js$/(file.name) && Copy(file.name, './dist/tests') );
+recursiveDir('./src', function(file) /_qa\.js$/(file.name) && copy(file.name, './dist/tests') );
 
-Copy('./tests/version.js', './dist/examples');
-Copy('./tests/helloworld.js', './dist/examples');
-Copy('./tests/systray.js', './dist/examples');
-Copy('./tests/miniInteractiveConsole.js', './dist/examples');
-Copy('./tests/3d.js', './dist/examples');
-Copy('./tests/ffiTest.js', './dist/examples');
-Copy('./tests/md5.js', './dist/examples');
-Copy('./tests/cs.js', './dist/examples');
-Copy('./tests/sqlite.js', './dist/examples');
-Copy('./tests/ode.js', './dist/examples');
-Copy('./tests/proceduralTextures.js', './dist/examples');
-Copy('./tests/oggFilePlayer.js', './dist/examples');
-Copy('./tests/41_30secOgg-q0.ogg', './dist/examples');
-Copy('./tests/testForDebugger.js', './dist/examples');
+copy('./tests/version.js', './dist/examples');
+copy('./tests/helloworld.js', './dist/examples');
+copy('./tests/systray.js', './dist/examples');
+copy('./tests/miniInteractiveConsole.js', './dist/examples');
+copy('./tests/3d.js', './dist/examples');
+copy('./tests/ffiTest.js', './dist/examples');
+copy('./tests/md5.js', './dist/examples');
+copy('./tests/cs.js', './dist/examples');
+copy('./tests/sqlite.js', './dist/examples');
+copy('./tests/ode.js', './dist/examples');
+copy('./tests/proceduralTextures.js', './dist/examples');
+copy('./tests/oggFilePlayer.js', './dist/examples');
+copy('./tests/41_30secOgg-q0.ogg', './dist/examples');
+copy('./tests/testForDebugger.js', './dist/examples');
 
-Copy('./tests/svg.js', './dist/examples');
-Copy('./src/jssvg/fonts.conf', './dist/examples');
+copy('./tests/svg.js', './dist/examples');
+copy('./src/jssvg/fonts.conf', './dist/examples');
 
-Copy('./src/jsdebug/debugger.js', './dist/bin');
-Copy('./src/jsdebug/debugger.xul', './dist/bin');
+copy('./src/jsdebug/debugger.js', './dist/bin');
+copy('./src/jsdebug/debugger.xul', './dist/bin');
 
-Copy('./src/jsprotex/liveconsole.xul', './dist/bin');
+copy('./src/jsprotex/liveconsole.xul', './dist/bin');
 
 
-RecursiveDir('./Win32_opt', function(file) /\.dll$/(file.name) && Copy(file.name, './dist/bin') );
+recursiveDir('./Win32_opt', function(file) /\.dll$/(file.name) && copy(file.name, './dist/bin') );
 
-Copy('./Win32_opt/jshost.exe', './dist/bin');
-Copy('./Win32_opt/jswinhost.exe', './dist/bin');
+copy('./Win32_opt/jshost.exe', './dist/bin');
+copy('./Win32_opt/jswinhost.exe', './dist/bin');
 
-Copy('./libs/openal/sdk/redist/OpenAL32.dll', './dist/bin');
-Copy('./libs/openal/sdk/redist/wrap_oal.dll', './dist/bin');
+copy('./libs/openal/sdk/redist/OpenAL32.dll', './dist/bin');
+copy('./libs/openal/sdk/redist/wrap_oal.dll', './dist/bin');
 
 new File('./dist/README.TXT').content = readme;
 copiedFilesDest.push('./dist/README.TXT');
 
 
-Zip('./dist/*', 'jslibs_'+type+'_'+jslibsVersion+'_'+'r'+jslibsRevision+'.zip');
+zip('./dist/*', 'jslibs_'+type+'_'+jslibsVersion+'_'+'r'+jslibsRevision+'.zip');
 
 // cleanup
 
 for each ( var f in copiedFilesDest )
-	new File(f).Delete()
+	new File(f).delete()
 	
