d9e6c16117280314f07b1ea11f257e0ecfd3d97a
soubok <soubok@aeb41dee-9b1b-0410-8513-d1797435cabc>
2012-06-02 12:45:14 +0000

    - fix __DATE__MONTH macro.

--- src/common/jlplatform.h
+++ src/common/jlplatform.h
@@ -38,7 +38,7 @@
 #define JL_CODE_LOCATION __FILE__ ":" JL_TOSTRING(__LINE__)
 
 #define __DATE__YEAR ((((__DATE__ [7] - '0') * 10 + (__DATE__ [8] - '0')) * 10 + (__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0'))
-#define __DATE__MONTH ((__DATE__ [2] == 'n' ? 0 : __DATE__ [2] == 'b' ? 1 : __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? 2 : 3) : __DATE__ [2] == 'y' ? 4 : __DATE__ [2] == 'n' ? 5 : __DATE__ [2] == 'l' ? 6 : __DATE__ [2] == 'g' ? 7 : __DATE__ [2] == 'p' ? 8 : __DATE__ [2] == 't' ? 9 : __DATE__ [2] == 'v' ? 10 : 11)+1)
+#define __DATE__MONTH ( (__DATE__[2] == 'n') ? (__DATE__[1] == 'a' ? 1 : 6) : (__DATE__[2] == 'b') ? 2 : (__DATE__[2] == 'r') ? (__DATE__[1] == 'a' ? 3 : 4) : (__DATE__[2] == 'y') ? 5 : (__DATE__[2] == 'l') ? 7 : (__DATE__[2] == 'g') ? 8 : (__DATE__[2] == 'p') ? 9 : (__DATE__[2] == 't') ? 10 : (__DATE__[2] == 'v') ? 11 : 12)
 #define __DATE__DAY ((__DATE__ [4] == ' ' ? 0 : __DATE__ [4] - '0') * 10 + (__DATE__ [5] - '0'))
 #define __DATE__HOUR (((__TIME__[0]-'0')*10) + (__TIME__[1]-'0'))
 #define __DATE__MINUTE (((__TIME__[3]-'0')*10) + (__TIME__[4]-'0'))
